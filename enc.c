4330a68e417b5ad0f29692336dcad407a831ceba
Martin Pool <mbp@sourcefrog.net>
2000-04-07 07:50:45 +0000

    Doc. Try to fix problems with different block sizes between the old and new signatures.

--- enc.c
+++ enc.c
@@ -1,68 +1,68 @@
 /* -*- mode: c; c-file-style: "k&r" -*- */
 
 /*
- * librsync: combined encode and sign
- * 
- * Copyright (C) 1999-2000 by Martin Pool. Copyright (C) 1999-2000 by Peter
- * Barker. Copyright (C) 1999 by Andrew Tridgell
- * 
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free 
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc., 59 
- * Temple Place, Suite 330, Boston, MA 02111-1307 USA 
- */
+   librsync: combined encode and sign
+   
+   Copyright (C) 1999-2000 by Martin Pool.
+   Copyright (C) 1999-2000 by Peter Barker.
+   Copyright (C) 1999 by Andrew Tridgell
+   
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the Free 
+   Software Foundation; either version 2 of the License, or (at your option)
+   any later version.
+   
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+   for more details.
+   
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc., 59 
+   Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+  */
 
 /*
- * ------------------
- * 
- * This file has an implementation of combined encoding & signing. The result 
- * is a single token stream which contains the changes relative to the old
- * signature and also the new signature.
- * 
- * ** ENCODING ** Encodes part of the INBUF at I.
- * 
- * If we recognize a block whose signature we know at that point, then we
- * skip the read cursor over the whole block and write out its token.
- * 
- * If we don't, then we just emit a single byte and will try again at the
- * next position.
- * 
- * We make sure that there is more than a block of readahead data available
- * in inbuf, unless we're approaching the end of the file.
- * 
- * If we're approaching the end of the file and there is less than block_size 
- * left, then we can still send a short block.  The checksum in this case is
- * taken just over the remaining part of the file.
- * 
- * When we first start processing a stream, we have to kickstart the weak
- * checksum by summing the whole first block through _rs_calc_weak_sum.
- * After this, though, we can just let the checksum accumulate by adding the
- * byte at the end of the block and letting older bytes fall off the end.  We 
- * will need to calculate a whole-block checksum again after outputting a
- * token, because we've skipped over the block whose state we held. 
- */
+   This file has an implementation of combined encoding & signing. The result 
+   is a single token stream which contains the changes relative to the old
+   signature and also the new signature.
+   
+   If we recognize a block whose signature we know at that point, then we
+   skip the read cursor over the whole block and write out its token.
+   
+   If we don't, then we just emit a single byte and will try again at the
+   next position.
+   
+   We make sure that there is more than a block of readahead data available
+   in inbuf, unless we're approaching the end of the file.
+   
+   If we're approaching the end of the file and there is less than block_size 
+   left, then we can still send a short block.  The checksum in this case is
+   taken just over the remaining part of the file.
+   
+   When we first start processing a stream, we have to kickstart the weak
+   checksum by summing the whole first block through _rs_calc_weak_sum.
+   After this, though, we can just let the checksum accumulate by adding the
+   byte at the end of the block and letting older bytes fall off the end.  We 
+   will need to calculate a whole-block checksum again after outputting a
+   token, because we've skipped over the block whose state we held.
 
+   For simplicity, we use the same block size for both the old and new
+   checksums.
 
-/*
- * As we read input from upstream, we have to send it into the
- * encoding process and also use it to generate a new signature.  We
- * never worry about caching the information because if we're encoding
- * we expect to encode again in the future, and the encoder never
- * needs the old value.  
- * 
- * Fuck!  Software patents are *such* a stupid idea.  It's really a
- * bit simpler to just do the signature on the client, but we can make
- * do with this.  */
+   We can also encode & checksum without an old signature returned
+   from the client.  We revert to this mode if anything goes wrong
+   with the clients signature, so this basically makes us fail-safe:
+   if they scrambled or damaged their signature then they ought to
+   just get a copy of the new data.
 
+   As we read input from upstream, we have to send it into the
+   encoding process and also use it to generate a new signature.  We
+   never worry about caching the information because if we're encoding
+   we expect to encode again in the future, and the encoder never
+   needs the old value.  
+   
+*/
 
 
 /* FIXME: Check that we use signed/unsigned integers properly.
@@ -97,13 +97,6 @@ _hs_newsig_header(int new_block_len,
 }
 
 
-
-/*
- * Current format:
- * 4 bytes for version number
- * 4 bytes for block length 
- * (int checksum1; byte[SUM_LENGTH] checksum2)* 
- */
 static int
 _hs_update_sums(inbuf_t * inbuf, int this_block_len, rollsum_t * rollsum)
 {
@@ -256,7 +249,7 @@ hs_encode(rs_read_fn_t read_fn, void *readprivate,
     int ret;
     rollsum_t real_rollsum, *const rollsum = &real_rollsum;
     inbuf_t real_inbuf, *const inbuf = &real_inbuf;
-    int old_block_len, this_block_len;
+    int block_len, shortened_block_len;
     hs_membuf_t *sig_tmpbuf, *lit_tmpbuf;
     int token;
     int at_eof;
@@ -274,22 +267,19 @@ hs_encode(rs_read_fn_t read_fn, void *readprivate,
 	 got_old = 0;
 
     if (got_old) {
-	 if (_hs_read_blocksize(sigread_fn, sigreadprivate, &old_block_len) < 0)
+	 if (_hs_read_blocksize(sigread_fn, sigreadprivate, &block_len) < 0)
 	      got_old = 0;
-	 
-	 if (!new_block_len)
-	      new_block_len = old_block_len;
     }
 
-    if (!new_block_len)
-	 new_block_len = 512;
+    if (!block_len)
+	 block_len = 512;
     
-    return_val_if_fail(new_block_len > 0, -1);
+    return_val_if_fail(block_len > 0, -1);
 
     if (got_old) {
 	 /* Put the char * sigbuffer into our structures */
 	 ret = _hs_make_sum_struct(&sums, sigread_fn, sigreadprivate,
-				   old_block_len);
+				   block_len);
 	 if (ret < 0)
 	      got_old = 0;
     }
@@ -300,9 +290,9 @@ hs_encode(rs_read_fn_t read_fn, void *readprivate,
     /* Allocate a buffer to hold literal output */
     sig_tmpbuf = hs_membuf_new();
     lit_tmpbuf = hs_membuf_new();
-    _hs_alloc_inbuf(inbuf, old_block_len);
+    _hs_alloc_inbuf(inbuf, block_len);
 
-    ret = _hs_newsig_header(new_block_len, hs_membuf_write, sig_tmpbuf);
+    ret = _hs_newsig_header(block_len, hs_membuf_write, sig_tmpbuf);
     if (ret < 0)
 	goto out;
 
@@ -320,11 +310,11 @@ hs_encode(rs_read_fn_t read_fn, void *readprivate,
 	 */
 	while (at_eof
 	       ? inbuf->cursor < inbuf->amount
-	       : inbuf->cursor + old_block_len <= inbuf->amount) {
-	    this_block_len = MIN(old_block_len, inbuf->amount - inbuf->cursor);
-	    _hs_update_sums(inbuf, this_block_len, rollsum);
+	       : inbuf->cursor + block_len <= inbuf->amount) {
+	    shortened_block_len = MIN(block_len, inbuf->amount - inbuf->cursor);
+	    _hs_update_sums(inbuf, shortened_block_len, rollsum);
 
-	    if (_hs_signature_ready(inbuf, new_block_len)) {
+	    if (_hs_signature_ready(inbuf, block_len)) {
 		/*
 		 * TODO: Maybe flush signature or literal data in here
 		 * somewhere too?  Doing this avoids accumulating a lot of
@@ -334,11 +324,11 @@ hs_encode(rs_read_fn_t read_fn, void *readprivate,
 		 * just avoid going to a larger command. 
 		 */
 		_hs_output_block_hash(hs_membuf_write, sig_tmpbuf,
-				      inbuf, new_block_len, rollsum);
+				      inbuf, block_len, rollsum);
 	    }
 
 	    if (got_old)
-		 token = _hs_find_match(this_block_len, rollsum, inbuf, sums);
+		 token = _hs_find_match(shortened_block_len, rollsum, inbuf, sums);
 	    else
 		 token = 0;
 	    
@@ -355,18 +345,18 @@ hs_encode(rs_read_fn_t read_fn, void *readprivate,
 
 		/* Write the token */
 		ret = _hs_emit_copy(write_fn, write_priv,
-				    (token-1) * old_block_len, this_block_len,
+				    (token-1) * block_len, shortened_block_len,
 				    stats);
 		if (ret < 0)
 		    return -1;
 
-		inbuf->cursor += this_block_len;
+		inbuf->cursor += shortened_block_len;
 		rollsum->havesum = 0;
 	    } else {
 		/* Append this character to the outbuf */
 		ret = _hs_append_literal(lit_tmpbuf,
 					 inbuf->buf[inbuf->cursor]);
-		_hs_roll_sums(inbuf, rollsum, old_block_len);
+		_hs_roll_sums(inbuf, rollsum, block_len);
 		inbuf->cursor++;
 	    }
 	}
