2378d4c8622224b684c5dd848b0ac7bb80942869
Martin Pool <mbp@sourcefrog.net>
2000-04-09 13:15:50 +0000

    Add test-thrice, which makes sure that the generated signature is stable and independant of the basis signature.

---
+++ test-thrice.sh
@@ -0,0 +1,39 @@
+#! /bin/sh -pe
+
+# Regression test suite for libhsync.
+
+# Copyright (C) 2000 by Martin Pool
+
+# This one makes sure that the signature calculated for a file is
+# independant of the old file we're comparing it to.
+
+# `What I tell you three times is true'
+
+if [ "$srcdir" = "" ]
+then
+    srcdir=`dirname $0`
+fi
+srcdir=`cd $srcdir; pwd`
+
+PATH=$srcdir:$PATH
+cd $srcdir/test-thrice
+
+echo -n `basename $0` ' '
+
+data=../enc.c
+
+rm -f *.tmp
+
+hsencode $data lt.tmp /dev/null
+hsdecode $data sig01.tmp new01.tmp lt.tmp
+cmp $data new01.tmp
+
+hsencode $data lt.tmp sig01.tmp 
+hsdecode $data sig02.tmp new02.tmp lt.tmp
+cmp sig01.tmp sig02.tmp
+cmp $data new02.tmp
+
+hsencode $data lt.tmp sig02.tmp
+hsdecode $data sig03.tmp new03.tmp lt.tmp
+cmp sig02.tmp sig03.tmp
+cmp $data new03.tmp
