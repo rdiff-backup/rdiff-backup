46ec2746845ddc1bdbbec637f9b166ad5e8958cd
Martin Pool <mbp@sourcefrog.net>
2000-04-21 14:42:20 +0000

    Make mdfour_update queue up any waiting data, rather than assuming it's at the end every time.

--- mdfour.c
+++ mdfour.c
@@ -1,3 +1,4 @@
+/* -*- mode: c; c-file-style: "k&r"; c-basic-offset: 4 -*-  */
 /* 
    mdfour code from Samba
    Copyright (C) 2000 by Martin Pool
@@ -49,189 +50,205 @@
 #define ROUND3(a,b,c,d,k,s) a = lshift(a + H(b,c,d) + X[k] + 0x6ED9EBA1,s)
 
 /* this applies md4 to 64 byte chunks */
-static void
-hs_mdfour64(hs_mdfour_t *m, uint32 * M)
+static void hs_mdfour64(hs_mdfour_t * m, uint32 * M)
 {
-    int j;
-    uint32 AA, BB, CC, DD;
-    uint32 X[16];
-    uint32 A, B, C, D;
+     int j;
+     uint32 AA, BB, CC, DD;
+     uint32 X[16];
+     uint32 A, B, C, D;
 
-    for (j = 0; j < 16; j++)
-	X[j] = M[j];
+     for (j = 0; j < 16; j++)
+	  X[j] = M[j];
 
-    A = m->A;
-    B = m->B;
-    C = m->C;
-    D = m->D;
-    AA = A;
-    BB = B;
-    CC = C;
-    DD = D;
+     A = m->A;
+     B = m->B;
+     C = m->C;
+     D = m->D;
+     AA = A;
+     BB = B;
+     CC = C;
+     DD = D;
 
-    ROUND1(A, B, C, D, 0, 3);
-    ROUND1(D, A, B, C, 1, 7);
-    ROUND1(C, D, A, B, 2, 11);
-    ROUND1(B, C, D, A, 3, 19);
-    ROUND1(A, B, C, D, 4, 3);
-    ROUND1(D, A, B, C, 5, 7);
-    ROUND1(C, D, A, B, 6, 11);
-    ROUND1(B, C, D, A, 7, 19);
-    ROUND1(A, B, C, D, 8, 3);
-    ROUND1(D, A, B, C, 9, 7);
-    ROUND1(C, D, A, B, 10, 11);
-    ROUND1(B, C, D, A, 11, 19);
-    ROUND1(A, B, C, D, 12, 3);
-    ROUND1(D, A, B, C, 13, 7);
-    ROUND1(C, D, A, B, 14, 11);
-    ROUND1(B, C, D, A, 15, 19);
+     ROUND1(A, B, C, D, 0, 3);
+     ROUND1(D, A, B, C, 1, 7);
+     ROUND1(C, D, A, B, 2, 11);
+     ROUND1(B, C, D, A, 3, 19);
+     ROUND1(A, B, C, D, 4, 3);
+     ROUND1(D, A, B, C, 5, 7);
+     ROUND1(C, D, A, B, 6, 11);
+     ROUND1(B, C, D, A, 7, 19);
+     ROUND1(A, B, C, D, 8, 3);
+     ROUND1(D, A, B, C, 9, 7);
+     ROUND1(C, D, A, B, 10, 11);
+     ROUND1(B, C, D, A, 11, 19);
+     ROUND1(A, B, C, D, 12, 3);
+     ROUND1(D, A, B, C, 13, 7);
+     ROUND1(C, D, A, B, 14, 11);
+     ROUND1(B, C, D, A, 15, 19);
 
-    ROUND2(A, B, C, D, 0, 3);
-    ROUND2(D, A, B, C, 4, 5);
-    ROUND2(C, D, A, B, 8, 9);
-    ROUND2(B, C, D, A, 12, 13);
-    ROUND2(A, B, C, D, 1, 3);
-    ROUND2(D, A, B, C, 5, 5);
-    ROUND2(C, D, A, B, 9, 9);
-    ROUND2(B, C, D, A, 13, 13);
-    ROUND2(A, B, C, D, 2, 3);
-    ROUND2(D, A, B, C, 6, 5);
-    ROUND2(C, D, A, B, 10, 9);
-    ROUND2(B, C, D, A, 14, 13);
-    ROUND2(A, B, C, D, 3, 3);
-    ROUND2(D, A, B, C, 7, 5);
-    ROUND2(C, D, A, B, 11, 9);
-    ROUND2(B, C, D, A, 15, 13);
+     ROUND2(A, B, C, D, 0, 3);
+     ROUND2(D, A, B, C, 4, 5);
+     ROUND2(C, D, A, B, 8, 9);
+     ROUND2(B, C, D, A, 12, 13);
+     ROUND2(A, B, C, D, 1, 3);
+     ROUND2(D, A, B, C, 5, 5);
+     ROUND2(C, D, A, B, 9, 9);
+     ROUND2(B, C, D, A, 13, 13);
+     ROUND2(A, B, C, D, 2, 3);
+     ROUND2(D, A, B, C, 6, 5);
+     ROUND2(C, D, A, B, 10, 9);
+     ROUND2(B, C, D, A, 14, 13);
+     ROUND2(A, B, C, D, 3, 3);
+     ROUND2(D, A, B, C, 7, 5);
+     ROUND2(C, D, A, B, 11, 9);
+     ROUND2(B, C, D, A, 15, 13);
 
-    ROUND3(A, B, C, D, 0, 3);
-    ROUND3(D, A, B, C, 8, 9);
-    ROUND3(C, D, A, B, 4, 11);
-    ROUND3(B, C, D, A, 12, 15);
-    ROUND3(A, B, C, D, 2, 3);
-    ROUND3(D, A, B, C, 10, 9);
-    ROUND3(C, D, A, B, 6, 11);
-    ROUND3(B, C, D, A, 14, 15);
-    ROUND3(A, B, C, D, 1, 3);
-    ROUND3(D, A, B, C, 9, 9);
-    ROUND3(C, D, A, B, 5, 11);
-    ROUND3(B, C, D, A, 13, 15);
-    ROUND3(A, B, C, D, 3, 3);
-    ROUND3(D, A, B, C, 11, 9);
-    ROUND3(C, D, A, B, 7, 11);
-    ROUND3(B, C, D, A, 15, 15);
+     ROUND3(A, B, C, D, 0, 3);
+     ROUND3(D, A, B, C, 8, 9);
+     ROUND3(C, D, A, B, 4, 11);
+     ROUND3(B, C, D, A, 12, 15);
+     ROUND3(A, B, C, D, 2, 3);
+     ROUND3(D, A, B, C, 10, 9);
+     ROUND3(C, D, A, B, 6, 11);
+     ROUND3(B, C, D, A, 14, 15);
+     ROUND3(A, B, C, D, 1, 3);
+     ROUND3(D, A, B, C, 9, 9);
+     ROUND3(C, D, A, B, 5, 11);
+     ROUND3(B, C, D, A, 13, 15);
+     ROUND3(A, B, C, D, 3, 3);
+     ROUND3(D, A, B, C, 11, 9);
+     ROUND3(C, D, A, B, 7, 11);
+     ROUND3(B, C, D, A, 15, 15);
 
-    A += AA;
-    B += BB;
-    C += CC;
-    D += DD;
+     A += AA;
+     B += BB;
+     C += CC;
+     D += DD;
 
 #ifdef LARGE_INT32
-    A &= 0xFFFFFFFF;
-    B &= 0xFFFFFFFF;
-    C &= 0xFFFFFFFF;
-    D &= 0xFFFFFFFF;
+     A &= 0xFFFFFFFF;
+     B &= 0xFFFFFFFF;
+     C &= 0xFFFFFFFF;
+     D &= 0xFFFFFFFF;
 #endif
 
-    for (j = 0; j < 16; j++)
-	X[j] = 0;
+     for (j = 0; j < 16; j++)
+	  X[j] = 0;
 
-    m->A = A;
-    m->B = B;
-    m->C = C;
-    m->D = D;
+     m->A = A;
+     m->B = B;
+     m->C = C;
+     m->D = D;
 }
 
 static void copy64(uint32 * M, unsigned char const *in)
 {
-    int i;
+     int i;
 
-    for (i = 0; i < 16; i++)
-	M[i] = (in[i * 4 + 3] << 24) | (in[i * 4 + 2] << 16) |
-	    (in[i * 4 + 1] << 8) | (in[i * 4 + 0] << 0);
+     for (i = 0; i < 16; i++)
+	  M[i] = (in[i * 4 + 3] << 24) | (in[i * 4 + 2] << 16) |
+	       (in[i * 4 + 1] << 8) | (in[i * 4 + 0] << 0);
 }
 
 static void copy4(unsigned char *out, uint32 x)
 {
-    out[0] = x & 0xFF;
-    out[1] = (x >> 8) & 0xFF;
-    out[2] = (x >> 16) & 0xFF;
-    out[3] = (x >> 24) & 0xFF;
-}
-
-void hs_mdfour_begin(hs_mdfour_t *md)
-{
-    md->A = 0x67452301;
-    md->B = 0xefcdab89;
-    md->C = 0x98badcfe;
-    md->D = 0x10325476;
-    md->totalN = 0;
+     out[0] = x & 0xFF;
+     out[1] = (x >> 8) & 0xFF;
+     out[2] = (x >> 16) & 0xFF;
+     out[3] = (x >> 24) & 0xFF;
 }
 
 
-static void hs_mdfour_tail(hs_mdfour_t *m, unsigned char const *in, int n)
+
+void hs_mdfour_begin(hs_mdfour_t * md)
 {
-    unsigned char buf[128];
-    uint32 M[16];
-    uint32 b;
-
-    m->totalN += n;
-
-    b = m->totalN * 8;
-
-    memset(buf, 0, 128);
-    if (n)
-	memcpy(buf, in, n);
-    buf[n] = 0x80;
-
-    if (n <= 55) {
-	copy4(buf + 56, b);
-	copy64(M, buf);
-	hs_mdfour64(m, M);
-    } else {
-	copy4(buf + 120, b);
-	copy64(M, buf);
-	hs_mdfour64(m, M);
-	copy64(M, buf + 64);
-	hs_mdfour64(m, M);
-    }
-}
-
-void
-hs_mdfour_update(hs_mdfour_t *md, unsigned char const *in, int n)
-{
-    uint32 M[16];
-
-    if (n == 0)
-	hs_mdfour_tail(md, in, n);
-
-    while (n >= 64) {
-	copy64(M, in);
-	hs_mdfour64(md, M);
-	in += 64;
-	n -= 64;
-	md->totalN += 64;
-    }
-
-    if (n)
-	hs_mdfour_tail(md, in, n);
+     memset(md, 0, sizeof(*md));
+     md->A = 0x67452301;
+     md->B = 0xefcdab89;
+     md->C = 0x98badcfe;
+     md->D = 0x10325476;
+     md->totalN = 0;
 }
 
 
-void
-hs_mdfour_result(hs_mdfour_t *md, unsigned char *out)
+static void hs_mdfour_tail(hs_mdfour_t * m, unsigned char const *in, int n)
 {
-    copy4(out, md->A);
-    copy4(out + 4, md->B);
-    copy4(out + 8, md->C);
-    copy4(out + 12, md->D);
+     unsigned char buf[128];
+     uint32 M[16];
+     uint32 b;
+
+     m->totalN += n;
+
+     b = m->totalN * 8;
+
+     memset(buf, 0, 128);
+     if (n)
+	  memcpy(buf, in, n);
+     buf[n] = 0x80;
+
+     if (n <= 55) {
+	  copy4(buf + 56, b);
+	  copy64(M, buf);
+	  hs_mdfour64(m, M);
+     } else {
+	  copy4(buf + 120, b);
+	  copy64(M, buf);
+	  hs_mdfour64(m, M);
+	  copy64(M, buf + 64);
+	  hs_mdfour64(m, M);
+     }
+}
+
+void hs_mdfour_update(hs_mdfour_t * md, unsigned char const *in, int n)
+{
+     uint32 M[16];
+     int n2 = 64 - md->tail_len;
+
+     if (n == 0) return;
+
+     if (n2 > n) n2 = n;
+     memcpy(&md->tail[md->tail_len], in, n2);
+     md->tail_len += n2;
+     in += n2;
+     n -= n2;
+
+     if (md->tail_len != 64) return;
+
+     copy64(M, md->tail);
+     hs_mdfour64(md, M);
+     md->tail_len = 0;
+     md->totalN += 64;
+
+     while (n >= 64) {
+	  copy64(M, in);
+	  hs_mdfour64(md, M);
+	  in += 64;
+	  n -= 64;
+	  md->totalN += 64;
+     }
+
+     if (n) {
+	  memcpy(md->tail, in, n);
+	  md->tail_len = n;
+     }
 }
 
 
-void
-hs_mdfour(unsigned char *out, unsigned char const *in, int n)
+void hs_mdfour_result(hs_mdfour_t * md, unsigned char *out)
 {
-    hs_mdfour_t md;
-    hs_mdfour_begin(&md);
-    hs_mdfour_update(&md, in, n);
-    hs_mdfour_result(&md, out);
+     hs_mdfour_tail(md, md->tail, md->tail_len);
+
+     copy4(out, md->A);
+     copy4(out + 4, md->B);
+     copy4(out + 8, md->C);
+     copy4(out + 12, md->D);
+}
+
+
+void hs_mdfour(unsigned char *out, unsigned char const *in, int n)
+{
+     hs_mdfour_t md;
+     hs_mdfour_begin(&md);
+     hs_mdfour_update(&md, in, n);
+     hs_mdfour_result(&md, out);
 }
