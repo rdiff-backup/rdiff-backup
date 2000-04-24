d659d8b33c978c288609ec2349b51b6ba8fccba7
Martin Pool <mbp@sourcefrog.net>
2000-04-24 14:22:58 +0000

    Clear up spare variable in _hs_make_sum_struct, and add more error checking.

--- checksum.c
+++ checksum.c
@@ -33,22 +33,22 @@ int checksum_seed = 0;
   */
 uint32_t _hs_calc_weak_sum(char const *buf1, int len)
 {
-    int i;
-    uint32_t s1, s2;
-    int8_t *buf = (uint8_t *) buf1;	/* this is signed */
+     int i;
+     uint32_t s1, s2;
+     int8_t *buf = (uint8_t *) buf1;	/* this is signed */
 
-    s1 = s2 = 0;
-    for (i = 0; i < (len - 4); i += 4) {
-	s2 += 4 * (s1 + buf[i]) + 3 * buf[i + 1] +
-	    2 * buf[i + 2] + buf[i + 3] + 10 * CHAR_OFFSET;
-	s1 += (buf[i + 0] + buf[i + 1] + buf[i + 2] + buf[i + 3] +
-	       4 * CHAR_OFFSET);
-    }
-    for (; i < len; i++) {
-	s1 += (buf[i] + CHAR_OFFSET);
-	s2 += s1;
-    }
-    return (s1 & 0xffff) + (s2 << 16);
+     s1 = s2 = 0;
+     for (i = 0; i < (len - 4); i += 4) {
+	  s2 += 4 * (s1 + buf[i]) + 3 * buf[i + 1] +
+	       2 * buf[i + 2] + buf[i + 3] + 10 * CHAR_OFFSET;
+	  s1 += (buf[i + 0] + buf[i + 1] + buf[i + 2] + buf[i + 3] +
+		 4 * CHAR_OFFSET);
+     }
+     for (; i < len; i++) {
+	  s1 += (buf[i] + CHAR_OFFSET);
+	  s2 += s1;
+     }
+     return (s1 & 0xffff) + (s2 << 16);
 }
 
 
@@ -66,16 +66,16 @@ uint32_t _hs_calc_weak_sum(char const *buf1, int len)
 uint32_t
 _hs_calc_strong_sum(char const *buf, int len, char *sum)
 {
-    hs_mdfour_t m;
-    char tsum[MD4_LENGTH];
+     hs_mdfour_t m;
+     char tsum[MD4_LENGTH];
 
-    hs_mdfour_begin(&m);
-    hs_mdfour_update(&m, (char *) buf, len);
-    hs_mdfour_result(&m, (char *) tsum);
+     hs_mdfour_begin(&m);
+     hs_mdfour_update(&m, (char *) buf, len);
+     hs_mdfour_result(&m, (char *) tsum);
 
-    memcpy(sum, tsum, SUM_LENGTH);
+     memcpy(sum, tsum, SUM_LENGTH);
 
-    return 0;
+     return 0;
 }
 
 
@@ -89,76 +89,76 @@ _hs_make_sum_struct(struct sum_struct **signatures,
 		    hs_read_fn_t sigread_fn, void *sigreadprivate,
 		    int block_len)
 {
-    struct sum_buf *asignature;
-    int count = 0;
-    int index = 0;
-    int ret = 0;
-    int checksum1;
-    struct sum_struct *sumbuf;
+     struct sum_buf *asignature;
+     int index = 0;
+     int ret = 0;
+     int checksum1;
+     struct sum_struct *sumbuf;
 
-    sumbuf = *signatures = calloc(1, sizeof(struct sum_struct));
-    if (!sumbuf) {
-	errno = ENOMEM;
-	return -1;
-    }
-    sumbuf->n = block_len;
+     sumbuf = *signatures = calloc(1, sizeof(struct sum_struct));
+     if (!sumbuf) {
+	  errno = ENOMEM;
+	  return -1;
+     }
+     sumbuf->n = block_len;
 
-    sumbuf->sums = NULL;
-    /* XXX: It's perhaps a bit inefficient to realloc each time.
-       We could prealloc, but for now we'll give realloc the
-       benefit of the doubt. */
+     sumbuf->sums = NULL;
+     /* XXX: It's perhaps a bit inefficient to realloc each time.
+	We could prealloc, but for now we'll give realloc the
+	benefit of the doubt. */
 
-    while (1) {
-	 ret = _hs_read_netint(sigread_fn, sigreadprivate, &checksum1);
+     while (1) {
+	  ret = _hs_read_netint(sigread_fn, sigreadprivate, &checksum1);
 
-	 if (ret == 0)
-	      break;
-	 if (ret < -1) {
-	      _hs_error("IO error while reading in signatures");
-	      return -1;
-	 }
-	 assert(ret == 4);
+	  if (ret == 0)
+	       break;
+	  if (ret < 0) {
+	       _hs_error("IO error while reading in signatures");
+	       return -1;
+	  }
+	  assert(ret == 4);
 	 
-	sumbuf->sums =
-	    realloc(sumbuf->sums, (count + 1) * sizeof(struct sum_buf));
-	if (sumbuf->sums == NULL) {
-	    errno = ENOMEM;
-	    ret = -1;
-	    break;
-	}
-	asignature = &(sumbuf->sums[count]);
+	  sumbuf->sums =
+	       realloc(sumbuf->sums, (index + 1) * sizeof(struct sum_buf));
+	  if (sumbuf->sums == NULL) {
+	       errno = ENOMEM;
+	       ret = -1;
+	       break;
+	  }
+	  asignature = &(sumbuf->sums[index]);
+	 
+	  asignature->sum1 = checksum1;
+	  asignature->i = ++index;
+	 
+	  /* read in the long sum */
+	  ret = _hs_must_read(sigread_fn, sigreadprivate, asignature->strong_sum, SUM_LENGTH);
+	  if (ret != SUM_LENGTH) {
+	       _hs_error("IO error while reading strong signature %d",
+			 index);
+	       break;
+	  }
+     }
+     if (ret < 0) {
+	  /* error reading */
+	  return ret;
+     }
 
-	asignature->sum1 = checksum1;
-	asignature->i = ++index;
+     sumbuf->count = index;
+     _hs_trace("Read %d sigs", index);
 
-	/* read in the long sum */
-	ret = _hs_read_loop(sigread_fn, sigreadprivate, asignature->strong_sum, SUM_LENGTH);
-	if (ret < 0)
-	    break;
+     ret = _hs_build_hash_table(*signatures) < 0;
 
-	count++;
-    }
-    if (ret < 0) {
-	/* error reading */
-	return ret;
-    }
-
-    sumbuf->count = count;
-    _hs_trace("Read %d sigs", count);
-
-    ret = _hs_build_hash_table(*signatures) < 0;
-
-    return ret;
+     return ret;
 }
 
 
 void _hs_free_sum_struct(struct sum_struct **psums)
 {
-    struct sum_struct *sums = *psums;
+     struct sum_struct *sums = *psums;
 
-    free(sums->sums);
-    /* XXX: I don't understand why we *don't* have to free this --
-       wasn't it allocated in make_sum_struct? -- mbp/20000125 */
-    //_hs_free(s, sums);
-    *psums = 0;
+     free(sums->sums);
+     /* XXX: I don't understand why we *don't* have to free this --
+	wasn't it allocated in make_sum_struct? -- mbp/20000125 */
+     //_hs_free(s, sums);
+     *psums = 0;
 }
