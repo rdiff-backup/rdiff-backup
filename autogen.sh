14438757087ffa34ac7234618c5eedc30a631c10
Martin Pool <mbp@sourcefrog.net>
2000-06-04 10:19:19 +0000

    Add a script to ease building from CVS.

---
+++ autogen.sh
@@ -0,0 +1,76 @@
+#! /bin/sh
+
+# $Id$
+
+# This script generates everything necessary to build libhsync from
+# CVS.  It is based on the file of the same name in glib.  It is not
+# needed for distributed versions, because all the necessary files are
+# included in the tarball by autoconf.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+PROJECT=libhsync
+TEST_TYPE=-f
+FILE=hsync.h
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.4.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+(libtool --version) </dev/null >/dev/null 2>&1 || {
+	echo
+	echo "You must have \`libtool' installed to compile $PROJECT."
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get it from ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+}
+
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+case $CC in
+*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
+esac
+
+aclocal $ACLOCAL_FLAGS
+
+# optionally feature autoheader
+(autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
+
+automake -a $am_opt
+autoconf
+cd $ORIGDIR
+
+$srcdir/configure --enable-maintainer-mode "$@"
+
+echo 
+echo "Now type 'make' to compile $PROJECT."
+
