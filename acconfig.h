8ab040b96d014e8e033c94e35c4d4fc671137c09
Martin Pool <mbp@sourcefrog.net>
2000-06-12 06:20:59 +0000

    Try to fix the handling of sockets on Solaris.

--- acconfig.h
+++ acconfig.h
@@ -1,4 +1,4 @@
-/* acconfig.h -- hand-written definitions to eventually go into config.h */
+\/* acconfig.h -- hand-written definitions to eventually go into config.h */
 
 /* Define this to enable trace code */
 #undef DO_HS_TRACE
@@ -8,3 +8,6 @@
 
 /* Define this if your sockaddr structure contains sin_len */
 #undef HAVE_SOCK_SIN_LEN
+
+/* Define this if there is a connect(2) call */
+#undef HAVE_CONNECT
