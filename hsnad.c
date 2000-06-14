f03794de986fad334b6b1885b8a9102db3618d20
Martin Pool <mbp@sourcefrog.net>
2000-06-14 01:06:04 +0000

    Doc.

--- hsnad.c
+++ hsnad.c
@@ -1,15 +1,24 @@
-/* -*- c-file-style: "bsd" -*- * * $Id: hsnad.c,v 1.2 2000/06/01 03:30:38 mbp 
-   Exp $ * * Copyright (C) 2000 by Martin Pool * * This program is free
-   software; you can redistribute it and/or modify * it under the terms of
-   the GNU General Public License as published by * the Free Software
-   Foundation; either version 2 of the License, or * (at your option) any
-   later version. * * This program is distributed in the hope that it will
-   be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty
-   of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the * GNU
-   General Public License for more details. * * You should have received a
-   copy of the GNU General Public License * along with this program; if not,
-   write to the Free Software * Foundation, Inc., 675 Mass Ave, Cambridge, MA 
-   02139, USA. */
+/*				       	-*- c-file-style: "bsd" -*-
+ * rproxy -- dynamic caching and delta update in HTTP
+ * $Id$
+ * 
+ * Copyright (C) 1999, 2000 by Martin Pool
+ * Copyright (C) 1999 by Andrew Tridgell
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include "includes.h"
 
@@ -36,7 +45,6 @@ process_args(int argc, char **argv)
 {
     int             c;
 
-    		/* may turn it on later */
     while ((c = getopt(argc, argv, "DS")) != -1) {
 	switch (c) {
 	case '?':
