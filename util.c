deb95f8d765af4aa86041c63c4757b4e36f86a45
Martin Pool <mbp@sourcefrog.net>
2000-08-09 02:11:52 +0000

    Moving towards 0.5.6pre2.

--- util.c
+++ util.c
@@ -1,19 +1,19 @@
-/*				       	-*- c-file-style: "bsd" -*-
+/*                                      -*- c-file-style: "bsd" -*-
  * rproxy -- dynamic caching and delta update in HTTP
  * $Id$
- * 
+ *
  * Copyright (C) 2000 by Martin Pool <mbp@humbug.org.au>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -37,7 +37,7 @@ _hs_alloc_struct0(size_t size, char const *name)
     void           *p;
 
     if (!(p = malloc(size))) {
-	_hs_fatal("couldn't allocate instance of %s", name);
+        _hs_fatal("couldn't allocate instance of %s", name);
     }
     hs_bzero(p, size);
     return p;
