77c77a8d461b9483d5fe3adf9160aa638b77864f
Martin Pool <mbp@sourcefrog.net>
2000-12-19 00:04:19 +0000

    Nonoptimal stream-based mksum code.

--- mksum.c
+++ mksum.c
@@ -4,7 +4,7 @@
  * $Id$
  * 
  * Copyright (C) 1999, 2000 by Martin Pool <mbp@linuxcare.com.au>
- * Copyright (C) 1999 by Andrew Tridgell
+ * Copyright (C) 1999 by Andrew Tridgell <tridge@samba.org>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -23,8 +23,7 @@
 
 
 /* 
- * Generate a checksum set, using the newstyle nonblocking
- * arrangement and mapptrs.
+ * mksum: Generate and write out checksums using the stream interface.
  */
 
 #include "config.h"
@@ -39,52 +38,111 @@
 #include "util.h"
 #include "protocol.h"
 #include "netint.h"
-
+#include "trace.h"
+#include "checksum.h"
 
 const int       HS_MKSUM_TAG = 123124;
 
 struct hs_mksum_job {
-    hs_stream_t    *stream;
-    int             dogtag;
-    size_t          block_len;
-    hs_stats_t      stats;
-    size_t          strong_sum_len;
-
-        int             (*statefn)(hs_mksum_job_t *);
+        int          dogtag;
+	hs_stream_t *stream;
+        enum hs_result (*statefn)(hs_mksum_job_t *);
+        size_t          block_len;
+        size_t          strong_sum_len;
+        int             near_end;
 };
 
 
+/* Possible state functions for signature generation. */
+static enum hs_result _hs_mksum_s_header(hs_mksum_job_t *);
+static enum hs_result _hs_mksum_s_complete(hs_mksum_job_t *);
+static enum hs_result _hs_mksum_s_generate(hs_mksum_job_t *);
+
+
+                                           
 /*
  * State of trying to send the signature header.
  */
-static int _hs_mksum_s_header(hs_mksum_job_t *job)
+static enum hs_result _hs_mksum_s_header(hs_mksum_job_t *job)
 {
         _hs_squirt_n32(job->stream, HS_SIG_MAGIC);
+        job->statefn = _hs_mksum_s_generate;
+
+        return HS_RUN_OK;
+}
+
+
+static enum hs_result
+_hs_mksum_do_block(hs_mksum_job_t *job, const void *block, size_t len)
+{
+        uint32_t weak_sum;
+
+        weak_sum = _hs_calc_weak_sum(block, len);
+        _hs_trace("got weak sum 0x%08x", weak_sum);
+
+        _hs_squirt_n32(job->stream, weak_sum);
+
+        return HS_RUN_OK;
+}
+
+
+/*
+ * State of reading a block and trying to generate its sum.
+ */
+static enum hs_result _hs_mksum_s_generate(hs_mksum_job_t *job)
+{
+        enum hs_result result;
+        int len;
+        void *block;
+        
+        /* must get a whole block, otherwise try again */
+        len = job->block_len;
+        result = _hs_scoop_read(job->stream, len, &block);
+        
+        /* unless we're near eof, in which case we'll accept
+         * whatever's in there */
+        if (result == HS_BLOCKED && job->near_end) {
+                result = _hs_scoop_read_rest(job->stream, &len, &block);
+                job->statefn = _hs_mksum_s_complete;
+        } else if (result != HS_OK) {
+                _hs_trace("generate stopped: %s", hs_strerror(result));
+                return result;
+        }
+
+        _hs_trace("got %d byte block", len);
+        fwrite(block, 1, len, stdout);
+
+        return _hs_mksum_do_block(job, block, len);
+}
+
+
+static enum hs_result _hs_mksum_s_complete(hs_mksum_job_t *UNUSED(job))
+{
+        _hs_trace("signature generation has already finished");
 
         return HS_OK;
 }
 
 
 /* Set up a new encoding job. */
-hs_mksum_job_t *
-hs_mksum_begin(hs_stream_t *stream,
-	       size_t new_block_len, size_t strong_sum_len)
+hs_mksum_job_t * hs_mksum_begin(hs_stream_t *stream,
+                                size_t new_block_len, size_t strong_sum_len)
 {
-    hs_mksum_job_t *job;
+        hs_mksum_job_t *job;
 
-    job = _hs_alloc_struct(hs_mksum_job_t);
+        job = _hs_alloc_struct(hs_mksum_job_t);
 
-    _hs_stream_check(stream);
-    job->stream = stream;
-    job->block_len = new_block_len;
-    job->dogtag = HS_MKSUM_TAG;
+        _hs_stream_check(stream);
+        job->stream = stream;
+        job->block_len = new_block_len;
+        job->dogtag = HS_MKSUM_TAG;
 
-    assert(strong_sum_len > 0 && strong_sum_len <= HS_MD4_LENGTH);
-    job->strong_sum_len = strong_sum_len;
+        assert(strong_sum_len > 0 && strong_sum_len <= HS_MD4_LENGTH);
+        job->strong_sum_len = strong_sum_len;
 
-    job->statefn = _hs_mksum_s_header;
+        job->statefn = _hs_mksum_s_header;
 
-    return job;
+        return job;
 }
 
 
@@ -101,21 +159,28 @@ int hs_mksum_finish(hs_mksum_job_t * job)
 
 
 /* 
- * Nonblocking iteration interface for making up a file sum. 
+ * Nonblocking iteration interface for making up a file sum.
+ *
+ * ENDING should be true if there is no more data after what's in the
+ * input buffer.  The final block checksum will run across whatever's
+ * in there, without trying to accumulate anything else.
  */
 int hs_mksum_iter(hs_mksum_job_t *job, int ending)
 {
-        int result;
+        enum hs_result result;
 
         assert(job->dogtag == HS_MKSUM_TAG);
 
-        do {
+        if (ending)
+                job->near_end = 1;
+
+        while (1) {
                 result = _hs_tube_catchup(job->stream);
                 if (result != HS_OK)
                         return result;
                 
                 result = job->statefn(job);
-        } while (result == HS_OK);
-
-        return result;
+                if (result != HS_RUN_OK)
+                        return result;
+        } 
 }
