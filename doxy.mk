297f4186386e8c24529166e74ed1426920f13423
Martin Pool <mbp@sourcefrog.net>
2001-02-26 10:25:12 +0000

    Include LaTeX stuff.

--- doxy.mk
+++ doxy.mk
@@ -2,8 +2,12 @@ sites=front.linuxcare.com.au:/var/www/projects/rproxy \
 	rproxy.sourceforge.net:/home/groups/rproxy/htdocs \
 	rproxy.samba.org:/space/httpd/rproxy/htdocs
 
-upload-doxy:
+upload-doxy: latex/refman.ps.gz
 	for i in $(sites); do \
-	rsync --delete -avz html/ rproxy-small-logo.png $$i/doxygen/libhsync; \
+	rsync --delete -avz html/ latex/refman.ps.gz ./rproxy-small-logo.png $$i/doxygen/libhsync; \
 	done
+
+latex/refman.ps.gz:
+	$(MAKE) -C latex refman.ps
+	gzip -9vf latex/refman.ps
 
