0fb856d1c3c6ab2918a4b796483884f25b7b0983
Martin Pool <mbp@sourcefrog.net>
2001-03-18 10:54:34 +0000

    Bias changes towards smaller sizes -- this is more realistic.
    Add a little user-friendliness.

--- mutate.pl
+++ mutate.pl
@@ -2,20 +2,20 @@
 
 # librsync -- the library for network deltas
 # $Id$
-# 
+#
 # Copyright (C) 1999, 2000 by Martin Pool <mbp@samba.org>
 # Copyright (C) 1999 by Andrew Tridgell <tridge@samba.org>
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License
 # as published by the Free Software Foundation; either version 2.1 of
 # the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -24,6 +24,11 @@
 # to stdout.
 
 use strict;
+
+if ($#ARGV != 1) {
+  print STDERR "usage: mutate.pl SEED MUTATIONS <BASIS\n";
+  exit 1;
+}
 
 srand shift @ARGV;
 my $n_muts = 1 + int rand shift @ARGV;
@@ -37,9 +42,9 @@ while ($n_muts-- > 0) {
   my $in_len = length($corpus);
 
   my $from_off = int rand $in_len;
-  my $from_len = int rand $in_len;
+  my $from_len = int(rand(1.0) * rand($in_len));
   my $to_off = int rand $in_len;
-  my $to_len = int rand $in_len;
+  my $to_len = int(rand(1.0) * rand($in_len));
 
   my $op = rand 3;
   if ($op < 1.0) {
