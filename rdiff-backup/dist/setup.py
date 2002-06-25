0becd23fc3ce9089d7b6469a1f61ddbe8e06a5fe
ben <ben@2b77aa54-bcbc-44c9-a7ec-4f6cf2b41109>
2002-06-25 18:02:55 +0000

    Futher distribution file improvements

--- rdiff-backup/dist/setup.py
+++ rdiff-backup/dist/setup.py
@@ -3,16 +3,11 @@
 import sys, os, getopt
 from distutils.core import setup, Extension
 
-version_string = "0.9.0"
+version_string = "$version"
 
 if sys.version_info[:2] < (2,2):
 	print "Sorry, rdiff-backup requires version 2.2 or later of python"
 	sys.exit(1)
-
-def install(cmd):
-	if os.system("install " + cmd) != 0:
-		print "Error running 'install %s'" % cmd
-		sys.exit(1)
 
 setup(name="rdiff-backup",
 	  version=version_string,
@@ -21,27 +16,11 @@ setup(name="rdiff-backup",
 	  author_email="bescoto@stanford.edu",
 	  url="http://rdiff-backup.stanford.edu",
 	  packages = ['rdiff_backup'],
-	  ext_modules=[Extension("rdiff_backup.C", ["cmodule.c"])])
+	  ext_modules = [Extension("rdiff_backup.C", ["cmodule.c"])],
+	  scripts = ['rdiff-backup'],
+	  data_files = [('share/man/man1', ['rdiff-backup.1']),
+					('share/doc/rdiff-backup-%s' % version_string,
+					 ['CHANGELOG', 'COPYING', 'README', 'FAQ.html'])])
 
-copy_files = 0
-prefix = "/usr"
-for arg in sys.argv[1:]: # check for "install" and --prefix= arg
-	if arg == "install": copy_files = 1
-	elif arg.startswith("--prefix="): prefix = arg[len("--prefix="):]
 
-if copy_files:
-	bindir = prefix + "/bin"
-	print "Copying rdiff-backup to " + bindir
-	install("-d " + bindir)
-	install("-m0755 rdiff-backup " + bindir)
-
-	mandir = prefix + "/share/man/man1"
-	print "Copying rdiff-backup.1 to " + mandir
-	install("-d " + mandir)
-	install("-m0644 rdiff-backup.1 " + mandir)
-
-	docdir = "%s/share/doc/rdiff-backup-%s" % (prefix, version_string)
-	print ("Copying CHANGELOG, COPYING, README, and FAQ.html to " + docdir)
-	install("-d " + docdir)
-	install("-m0644 CHANGELOG COPYING README FAQ.html " + docdir)
 
