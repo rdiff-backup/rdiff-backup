b03506d24d66f082b8b367b7d45a6b6b8c314d98
Wayne Davison <wayned@samba.org>
2002-06-29 19:47:42 +0000

    Define uint64_t, if possible.

--- types.h
+++ types.h
@@ -20,17 +20,29 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-
+#define HAVE_UINT64 1 /* assume success, but might be undefined below */
 
 #if defined(HAVE_STDINT_H)
 #  include <stdint.h>
-#elif SIZEOF_UNSIGNED_INT == 4
-#  define uint32_t unsigned int
-#elif SIZEOF_UNSIGNED_LONG == 4
-#  define uint32_t unsigned long
-#elif SIZEOF_UNSIGNED_SHORT == 4
-#  define uint32_t unsigned short
 #else
+# if SIZEOF_UNSIGNED_INT == 4
+#  define uint32_t unsigned int
+# elif SIZEOF_UNSIGNED_LONG == 4
+#  define uint32_t unsigned long
+# elif SIZEOF_UNSIGNED_SHORT == 4
+#  define uint32_t unsigned short
+# else
 #  error "can't find an appropriate 32-bit integer type"
+# endif
+# if SIZEOF_UNSIGNED_INT == 8
+#  define uint64_t unsigned int
+# elif SIZEOF_UNSIGNED_LONG == 8
+#  define uint64_t unsigned long
+# elif SIZEOF_UNSIGNED_SHORT == 8
+#  define uint64_t unsigned short
+# elif SIZEOF_LONG_LONG == 8
+#  define uint64_t unsigned long long
+# else
+#  undef HAVE_UINT64
+# endif
 #endif
-
