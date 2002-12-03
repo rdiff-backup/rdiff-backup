0388520935934fc191315a7fb2d3e6f5535b1a0c
ben <ben@2b77aa54-bcbc-44c9-a7ec-4f6cf2b41109>
2002-12-03 00:34:53 +0000

    Changed GPL notice in each source file to recommended version

--- rdiff-backup/rdiff_backup/static.py
+++ rdiff-backup/rdiff_backup/static.py
@@ -3,10 +3,19 @@
 # This file is part of rdiff-backup.
 #
 # rdiff-backup is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, Inc., 675 Mass Ave, Cambridge MA
-# 02139, USA; either version 2 of the License, or (at your option) any
-# later version; incorporated herein by reference.
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# rdiff-backup is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with rdiff-backup; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
 
 """MakeStatic and MakeClass
 
