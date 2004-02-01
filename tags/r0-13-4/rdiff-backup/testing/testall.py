9caf160dceb49b087b4a2f71beb29758acacc2d4
cvs2svn <cvs2svn@2b77aa54-bcbc-44c9-a7ec-4f6cf2b41109>
2004-02-01 05:04:06 +0000

    This commit was manufactured by cvs2svn to create tag 'r0-13-4'.

---
+++ tags/r0-13-4/rdiff-backup/testing/testall.py
@@ -0,0 +1,26 @@
+import unittest
+
+"""This probably doesn't work any more - just run the tests manually."""
+
+from connectiontest import *
+#from destructive-steppingtest import *
+from dstest import *
+from highleveltest import *
+from incrementtest import *
+from iterfiletest import *
+from lazytest import *
+from rdifftest import *
+from regressiontest import *
+from restoretest import *
+from rlisttest import *
+from rorpitertest import *
+from rpathtest import *
+#from finaltest import *
+from statictest import *
+from timetest import *
+from filelisttest import *
+from setconnectionstest import *
+
+if __name__ == "__main__":
+	unittest.main()
+
