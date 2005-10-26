ee9a3d308e374e3ea6f3d6e6b35c6827ffdf9859
bescoto <bescoto@2b77aa54-bcbc-44c9-a7ec-4f6cf2b41109>
2005-10-26 00:52:53 +0000

    typo fix

--- trunk/rdiff-backup/rdiff_backup/fs_abilities.py
+++ trunk/rdiff-backup/rdiff_backup/fs_abilities.py
@@ -436,8 +436,8 @@ class SetGlobals:
 		self.update_triple(self.src_fsa.carbonfile, self.dest_fsa.carbonfile,
 			  ('carbonfile_active', 'carbonfile_write', 'carbonfile_conn'))
 		if self.src_fsa.carbonfile and not Globals.carbonfile_active:
-			Log("Source may have carbonfile support, but support defaults to "
-				"off.\n  Use --carbonfile to enable.", 5)
+			log.Log("Source may have carbonfile support, but support "
+					"defaults to off.\n  Use --carbonfile to enable.", 5)
 
 	def set_hardlinks(self):
 		if Globals.preserve_hardlinks != 0:
