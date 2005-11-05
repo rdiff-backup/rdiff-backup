19bfa257d4e06cb75b225cff4dde5941be79ff0e
bescoto <bescoto@2b77aa54-bcbc-44c9-a7ec-4f6cf2b41109>
2005-11-05 02:56:21 +0000

    quoted filename too long fix

--- branches/r1-0/rdiff-backup/testing/FilenameMappingtest.py
+++ branches/r1-0/rdiff-backup/testing/FilenameMappingtest.py
@@ -1,6 +1,6 @@
 import unittest
 from commontest import *
-from rdiff_backup import FilenameMapping
+from rdiff_backup import FilenameMapping, rpath, Globals
 
 class FilenameMappingTest(unittest.TestCase):
 	"""Test the FilenameMapping class, for quoting filenames"""
@@ -28,5 +28,32 @@ class FilenameMappingTest(unittest.TestCase):
 		assert len(qrp.index) == 1, qrp.index
 		assert (qrp.index[0] ==
 				"mirror_metadata.1969-12-31T21:33:20-07:00.data.gz")
+
+	def testLongFilenames(self):
+		"""See if long quoted filenames cause crash"""
+		MakeOutputDir()
+		outrp = rpath.RPath(Globals.local_connection, "testfiles/output")
+		inrp = rpath.RPath(Globals.local_connection, "testfiles/quotetest")
+		re_init_dir(inrp)
+		long_filename = "A"*200 # when quoted should cause overflow
+		longrp = inrp.append(long_filename)
+		longrp.touch()
+		shortrp = inrp.append("B")
+		shortrp.touch()
+
+		rdiff_backup(1, 1, inrp.path, outrp.path, 100000,
+					 extra_options = "--override-chars-to-quote A")
+
+		longrp_out = outrp.append(long_filename)
+		assert not longrp_out.lstat()
+		shortrp_out = outrp.append('B')
+		assert shortrp_out.lstat()
+
+		rdiff_backup(1, 1, "testfiles/empty", outrp.path, 200000)
+		shortrp_out.setdata()
+		assert not shortrp_out.lstat()
+		rdiff_backup(1, 1, inrp.path, outrp.path, 300000)
+		shortrp_out.setdata()
+		assert shortrp_out.lstat()
 
 if __name__ == "__main__": unittest.main()
