686675988c5aff2c0d652afcc9621bd904e6001c
bescoto <bescoto@2b77aa54-bcbc-44c9-a7ec-4f6cf2b41109>
2005-11-26 04:56:15 +0000

    Final changes for 1.1.3

--- rdiff-backup/testing/metadatatest.py
+++ rdiff-backup/testing/metadatatest.py
@@ -1,6 +1,6 @@
 import unittest, os, cStringIO, time
-from rdiff_backup.metadata import *
 from rdiff_backup import rpath, connection, Globals, selection, lazy
+from rdiff_backup.metadata import *
 
 tempdir = rpath.RPath(Globals.local_connection, "testfiles/output")
 
