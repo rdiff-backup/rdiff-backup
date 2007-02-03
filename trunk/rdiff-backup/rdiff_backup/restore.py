ab881e17aa02a4fc3547ac8aff745c7e718dc282
dgaudet <dgaudet@2b77aa54-bcbc-44c9-a7ec-4f6cf2b41109>
2007-02-03 17:32:27 +0000

    Fix restore from read-only bug -- rx perms on a repository directory are enough, no need for write perms when restoring.  (patch from Andrew Price)

--- trunk/rdiff-backup/rdiff_backup/restore.py
+++ trunk/rdiff-backup/rdiff_backup/restore.py
@@ -708,7 +708,7 @@ class PermissionChanger:
 		"""Change permissions of directories between old_index and index"""
 		for rp in self.get_new_rp_list(old_index, index):
 			if ((rp.isreg() and not rp.readable()) or
-				(rp.isdir() and not rp.hasfullperms())):
+				(rp.isdir() and not (rp.executable() and rp.readable()))):
 				old_perms = rp.getperms()
 				self.open_index_list.insert(0, (rp.index, rp, old_perms))
 				if rp.isreg(): rp.chmod(0400 | old_perms)
