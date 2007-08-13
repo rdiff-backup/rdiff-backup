fba64fc2df28f1c896c823b9d16882ae24eb2767
owsla <owsla@2b77aa54-bcbc-44c9-a7ec-4f6cf2b41109>
2007-08-13 16:11:15 +0000

    More robust workaround for EFTYPE only being present on BSD

--- trunk/rdiff-backup/rdiff_backup/rpath.py
+++ trunk/rdiff-backup/rdiff_backup/rpath.py
@@ -832,8 +832,8 @@ class RPath(RORPath):
 		try:
 			self.conn.os.chmod(self.path, permissions & Globals.permission_mask)
 		except OSError, e:
-		    # XXX: BSD defines EFTYPE as 79, but it is not in python
-			if e.errno == 79 and not self.isdir():
+			if e.strerror == "Inappropriate file type or format" \
+					and not self.isdir():
 				# Some systems throw this error if try to set sticky bit
 				# on a non-directory. Remove sticky bit and try again.
 				log.Log("Unable to set permissions of %s to %o - trying again"
