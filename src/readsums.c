28d7bfb935ec542980ea4fa37afc0bddb506263e
Martin Pool <mbp@sourcefrog.net>
2015-12-03 22:35:33 -0800

    Add a Doxygen link

--- src/readsums.c
+++ src/readsums.c
@@ -1,20 +1,20 @@
 /*=                     -*- c-basic-offset: 4; indent-tabs-mode: nil; -*-
  *
  * librsync -- the library for network deltas
- * 
+ *
  * Copyright (C) 1999, 2000, 2001 by Martin Pool <mbp@sourcefrog.net>
  * Copyright (C) 1999 by Andrew Tridgell <tridge@samba.org>
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -203,7 +203,7 @@ static rs_result rs_loadsig_s_magic(rs_job_t *job)
  * the file.
  *
  * \note After loading the signatures, you must call
- * rs_build_hash_table() before you can use them.
+ * ::rs_build_hash_table() before you can use them.
  */
 rs_job_t *rs_loadsig_begin(rs_signature_t **signature)
 {
@@ -215,4 +215,3 @@ rs_job_t *rs_loadsig_begin(rs_signature_t **signature)
 
     return job;
 }
-
