86fa2bf7b5915fed9ebf18c875209abbb29a159d
Martin Pool <mbp@sourcefrog.net>
2015-12-03 22:32:11 -0800

    Mark _Rollsum private in Doxygen

--- src/rollsum.h
+++ src/rollsum.h
@@ -1,20 +1,20 @@
 /*= -*- c-basic-offset: 4; indent-tabs-mode: nil; -*-
  *
  * rollsum -- the librsync rolling checksum
- * 
- * Copyright (C) 2003 by Donovan Baarda <abo@minkirri.apana.org.au> 
+ *
+ * Copyright (C) 2003 by Donovan Baarda <abo@minkirri.apana.org.au>
  * based on work, Copyright (C) 2000, 2001 by Martin Pool <mbp@sourcefrog.net>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -27,6 +27,8 @@
 #define ROLLSUM_CHAR_OFFSET 31
 
 /* the Rollsum struct type*/
+
+/** \private */
 typedef struct _Rollsum {
     unsigned long count;               /* count of bytes included in sum */
     unsigned long s1;                  /* s1 part of sum */
