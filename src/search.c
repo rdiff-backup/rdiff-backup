1e495b6bb4c2f1500c71f0eb54665bee9fd2ca19
Martin Pool <mbp@sourcefrog.net>
2015-12-26 21:20:26 -0800

    https://www.jwz.org/doc/tabs-vs-spaces.html

--- src/search.c
+++ src/search.c
@@ -64,46 +64,46 @@ static void swap(rs_target_t *t1, rs_target_t *t2) {
 static int rs_compare_targets(rs_target_t const *t1, rs_target_t const *t2, rs_signature_t * sums) {
     int v = (int) t1->t - (int) t2->t;
     if (v != 0)
-	return v;
+        return v;
 
     rs_weak_sum_t w1 = sums->block_sigs[t1->i].weak_sum;
     rs_weak_sum_t w2 = sums->block_sigs[t2->i].weak_sum;
 
     v = (w1 > w2) - (w1 < w2);
     if (v != 0)
-	return v;
+        return v;
 
     return memcmp(sums->block_sigs[t1->i].strong_sum,
-	    sums->block_sigs[t2->i].strong_sum,
-	    sums->strong_sum_len);
+            sums->block_sigs[t2->i].strong_sum,
+            sums->strong_sum_len);
 }
 
 static void heap_sort(rs_signature_t * sums) {
     unsigned int i, j, n, k, p;
     for (i = 1; i < sums->count; ++i) {
-	for (j = i; j > 0;) {
-	    p = (j - 1) >> 1;
-	    if (rs_compare_targets(&sums->targets[j], &sums->targets[p], sums) > 0)
-		swap(&sums->targets[j], &sums->targets[p]);
-	    else
-		break;
-	    j = p;
-	}
+        for (j = i; j > 0;) {
+            p = (j - 1) >> 1;
+            if (rs_compare_targets(&sums->targets[j], &sums->targets[p], sums) > 0)
+                swap(&sums->targets[j], &sums->targets[p]);
+            else
+                break;
+            j = p;
+        }
     }
 
     for (n = sums->count - 1; n > 0;) {
-	swap(&sums->targets[0], &sums->targets[n]);
-	--n;
-	for (i = 0; ((i << 1) + 1) <= n;) {
-	    k = (i << 1) + 1;
-	    if ((k + 1 <= n) && (rs_compare_targets(&sums->targets[k], &sums->targets[k + 1], sums) < 0))
-		k = k + 1;
-	    if (rs_compare_targets(&sums->targets[k], &sums->targets[i], sums) > 0)
-		swap(&sums->targets[k], &sums->targets[i]);
-	    else
-		break;
-	    i = k;
-	}
+        swap(&sums->targets[0], &sums->targets[n]);
+        --n;
+        for (i = 0; ((i << 1) + 1) <= n;) {
+            k = (i << 1) + 1;
+            if ((k + 1 <= n) && (rs_compare_targets(&sums->targets[k], &sums->targets[k + 1], sums) < 0))
+                k = k + 1;
+            if (rs_compare_targets(&sums->targets[k], &sums->targets[i], sums) > 0)
+                swap(&sums->targets[k], &sums->targets[i]);
+            else
+                break;
+            i = k;
+        }
     }
 }
 
@@ -117,32 +117,32 @@ rs_build_hash_table(rs_signature_t * sums)
         return RS_MEM_ERROR;
 
     if (sums->count > 0) {
-	sums->targets = calloc(sums->count, sizeof(rs_target_t));
+        sums->targets = calloc(sums->count, sizeof(rs_target_t));
         if (!sums->targets) {
-	    free(sums->tag_table);
-	    sums->tag_table = NULL;
+            free(sums->tag_table);
+            sums->tag_table = NULL;
             return RS_MEM_ERROR;
-	}
+        }
 
-	for (i = 0; i < sums->count; i++) {
-	    sums->targets[i].i = i;
-	    sums->targets[i].t = gettag(sums->block_sigs[i].weak_sum);
-	}
+        for (i = 0; i < sums->count; i++) {
+            sums->targets[i].i = i;
+            sums->targets[i].t = gettag(sums->block_sigs[i].weak_sum);
+        }
 
-	heap_sort(sums);
+        heap_sort(sums);
     }
 
     for (i = 0; i < TABLE_SIZE; i++) {
-	sums->tag_table[i].l = NULL_TAG;
-	sums->tag_table[i].r = NULL_TAG;
+        sums->tag_table[i].l = NULL_TAG;
+        sums->tag_table[i].r = NULL_TAG;
     }
 
     for (i = sums->count - 1; i >= 0; i--) {
-	sums->tag_table[sums->targets[i].t].l = i;
+        sums->tag_table[sums->targets[i].t].l = i;
     }
 
     for (i = 0; i < sums->count; i++) {
-	sums->tag_table[sums->targets[i].t].r = i;
+        sums->tag_table[sums->targets[i].t].r = i;
     }
 
     rs_trace("rs_build_hash_table done");
@@ -180,63 +180,63 @@ rs_search_for_block(rs_weak_sum_t weak_sum,
     int v = 1;
 
     if (l == NULL_TAG)
-	return 0;
+        return 0;
 
     while (l < r) {
-	int m = (l + r) >> 1;
-	int i = sig->targets[m].i;
-	rs_block_sig_t *b = &(sig->block_sigs[i]);
-	v = (weak_sum > b->weak_sum) - (weak_sum < b->weak_sum);
+        int m = (l + r) >> 1;
+        int i = sig->targets[m].i;
+        rs_block_sig_t *b = &(sig->block_sigs[i]);
+        v = (weak_sum > b->weak_sum) - (weak_sum < b->weak_sum);
         // v < 0  - weak_sum <  b->weak_sum
         // v == 0 - weak_sum == b->weak_sum
         // v > 0  - weak_sum >  b->weak_sum
 
-	if (v == 0) {
-	    if (!got_strong) {
-		if(sig->magic == RS_BLAKE2_SIG_MAGIC) {
-		    rs_calc_blake2_sum(inbuf, block_len, &strong_sum);
-		} else if (sig->magic == RS_MD4_SIG_MAGIC) {
-		    rs_calc_md4_sum(inbuf, block_len, &strong_sum);
-		} else {
-		    rs_error("Unknown signature algorithm - this is a BUG");
-		    return 0; /* FIXME: Is this the best way to handle this? */
-		}
-		got_strong = 1;
-	    }
-	    v = memcmp(strong_sum, b->strong_sum, sig->strong_sum_len);
+        if (v == 0) {
+            if (!got_strong) {
+                if(sig->magic == RS_BLAKE2_SIG_MAGIC) {
+                    rs_calc_blake2_sum(inbuf, block_len, &strong_sum);
+                } else if (sig->magic == RS_MD4_SIG_MAGIC) {
+                    rs_calc_md4_sum(inbuf, block_len, &strong_sum);
+                } else {
+                    rs_error("Unknown signature algorithm - this is a BUG");
+                    return 0; /* FIXME: Is this the best way to handle this? */
+                }
+                got_strong = 1;
+            }
+            v = memcmp(strong_sum, b->strong_sum, sig->strong_sum_len);
 
-	    if (v == 0) {
-		l = m;
-		r = m;
-		break;
-	    }
-	}
+            if (v == 0) {
+                l = m;
+                r = m;
+                break;
+            }
+        }
 
-	if (v > 0)
-	    l = m + 1;
-	else
-	    r = m;
+        if (v > 0)
+            l = m + 1;
+        else
+            r = m;
     }
 
     if (l == r) {
-	int i = sig->targets[l].i;
-	rs_block_sig_t *b = &(sig->block_sigs[i]);
-	if (weak_sum != b->weak_sum)
-	    return 0;
-	if (!got_strong) {
+        int i = sig->targets[l].i;
+        rs_block_sig_t *b = &(sig->block_sigs[i]);
+        if (weak_sum != b->weak_sum)
+            return 0;
+        if (!got_strong) {
             if(sig->magic == RS_BLAKE2_SIG_MAGIC) {
-	        rs_calc_blake2_sum(inbuf, block_len, &strong_sum);
-	    } else if (sig->magic == RS_MD4_SIG_MAGIC) {
+                rs_calc_blake2_sum(inbuf, block_len, &strong_sum);
+            } else if (sig->magic == RS_MD4_SIG_MAGIC) {
                 rs_calc_md4_sum(inbuf, block_len, &strong_sum);
-	    } else {
-	        rs_error("Unknown signature algorithm - this is a BUG");
-		return 0; /* FIXME: Is this the best way to handle this? */
-	    }
-	    got_strong = 1;
-	}
-	v = memcmp(strong_sum, b->strong_sum, sig->strong_sum_len);
-	int token = b->i;
-	*match_where = (rs_long_t)(token - 1) * sig->block_len;
+            } else {
+                rs_error("Unknown signature algorithm - this is a BUG");
+                return 0; /* FIXME: Is this the best way to handle this? */
+            }
+            got_strong = 1;
+        }
+        v = memcmp(strong_sum, b->strong_sum, sig->strong_sum_len);
+        int token = b->i;
+        *match_where = (rs_long_t)(token - 1) * sig->block_len;
     }
 
     return !v;
