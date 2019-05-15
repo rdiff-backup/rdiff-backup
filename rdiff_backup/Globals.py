31abd0e7ddde21fb28521d843842c9cf6bdd2bb8
Eric L <ewl+git@lavar.de>
2019-05-15 21:52:34 +0200

    Set correctly version in Globals to 1.3.4

--- rdiff_backup/Globals.py
+++ rdiff_backup/Globals.py
@@ -23,7 +23,7 @@ import re, os
 
 
 # The current version of rdiff-backup
-version = "$version"
+version = "1.3.4"
 
 # If this is set, use this value in seconds as the current time
 # instead of reading it from the clock.
