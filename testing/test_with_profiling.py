993992cd91768bd5e515c72788ec394e7118ba36
Eric L <ewl+git@lavar.de>
2019-05-31 18:09:27 +0200

    Add comments to test_with_profiling to make it more usable
    Actually it's more of a helper script to profile certain test suites
    after adaptation.

--- testing/test_with_profiling.py
+++ testing/test_with_profiling.py
@@ -1,7 +1,18 @@
 import profile, pstats
+
+# if you need to profile a certain test, replace the "metadatatest" placeholder
+# with the test you want to profile and all functions will get imported into
+# the current namespace.
+# I didn't find a way to do it dynamically...
 from metadatatest import *
 
-profile.run("unittest.main()", "profile-output")
-p = pstats.Stats("profile-output")
+# Create a profile output filename (don't forget to adapt the name)
+abs_work_dir = os.getenv('TOX_ENV_DIR', os.getenv('VIRTUAL_ENV',
+    os.path.join(os.getcwd(), 'build')))
+profile_output = os.path.join(abs_work_dir, "profile-metadatatest.out")
+
+# Run and output the test profile
+profile.run("unittest.main()", profile_output)
+p = pstats.Stats(profile_output)
 p.sort_stats('time')
 p.print_stats(40)
