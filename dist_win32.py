5f7674a54057e0a1920a02eafd7491ffa9a1ae5a
Arrigo Marchiori <ardovm@yahoo.it>
2019-10-02 17:51:56 +0200

    use pyinstaller instead of py2exe
    Add entries to .gitignore
    Synchronize submodule librsync

--- dist_win32.py
+++ dist_win32.py
@@ -7,6 +7,7 @@ import os
 import os.path
 import subprocess
 import zipfile
+import PyInstaller.__main__
 
 def runCommand(*args):
     """Launch a command and abort if not successful.
@@ -18,41 +19,43 @@ def runCommand(*args):
         sys.stderr.write("The command gave an error:\n  %s\n" % " ".join(args))
         sys.exit(1)
 
-if len(sys.argv) != 2:
-    sys.stderr.write("Usage: %s version\n" % sys.argv[0])
+if "--help" in sys.argv:
+    sys.stderr.write("Usage: %s [version]\n" % sys.argv[0])
     sys.exit(1)
-version = sys.argv[1]
+if len(sys.argv) > 1:
+    version = sys.argv[1]
+else:
+    from src.rdiff_backup.Version import version
 # Compilation of librsync
 os.chdir("librsync")
-runCommand("cmake", "-DCMAKE_BUILD_TYPE=Release",
-            "-DCMAKE_INSTALL_PREFIX=../librsync-bin")
-runCommand("nmake")
-runCommand("nmake", "install")
+runCommand("cmake", "-DCMAKE_INSTALL_PREFIX=../librsync-bin", "-A", "Win32", ".")
+runCommand("cmake", "--build", ".", "--config", "Release")
+runCommand("cmake", "--install", ".", "--config", "Release")
 os.chdir("..")
 # Build of rdiff-backup
-os.chdir("rdiff-backup")
-runCommand(sys.executable, os.path.join(".", "dist", "makedist"), "--no-tar",
-           version)
-os.chdir("rdiff-backup-%s" % version)
-if "VS90COMNTOOLS" not in os.environ:
-    if "VCINSTALLDIR" not in os.environ:
-        sys.stderr.write("Need either the VS90COMNTOOLS or VCINSTALLDIR "
-                         "environment variable.\n")
-        sys.exit(1)
-    os.putenv("VS90COMNTOOLS", os.path.join(os.environ["VCINSTALLDIR"], "bin"))
-os.putenv("LIBRSYNC_DIR", os.path.join("..", "..", "librsync-bin"))
+# runCommand(sys.executable, os.path.join(".", "dist", "makedist"), "--no-tar",
+#           version)
+# os.chdir("rdiff-backup-%s" % version)
+if "VS160COMNTOOLS" not in os.environ:
+    sys.stderr.write("VS160COMNTOOLS environment variable not set.\n")
+    sys.exit(1)
+os.putenv("LIBRSYNC_DIR", "librsync-bin")
 runCommand(sys.executable, "setup.py", "build")
-runCommand(sys.executable, "setup.py", "py2exe", "--single-file")
+PyInstaller.__main__.run(["--onefile",
+                          "--distpath=%s" % os.path.join("dist", "win32"),
+                          "--onefile",
+                          "--paths=%s" % os.path.join("build", "lib.win32-3.7"),
+                          "--paths=%s" % os.path.join("librsync-bin", "bin"),
+                          "--console", "rdiff-backup"])
+
 # Packaging of rdiff-backup
-with zipfile.ZipFile(os.path.join("..", "..", "rdiff-backup-%s.zip" % version),
-                     "w", zipfile.ZIP_DEFLATED) as z:
-    # All files in dist\
-    for f in os.listdir("dist"):
-        fWithPath = os.path.join("dist", f)
+with zipfile.ZipFile("rdiff-backup-%s.zip" % version, "w",
+                     zipfile.ZIP_DEFLATED) as z:
+    # All files under directory dist
+    for f in os.listdir(os.path.join("dist", "win32")):
+        fWithPath = os.path.join("dist", "win32", f)
         if os.path.isfile(fWithPath):
             z.write(fWithPath, f)
-    # All files under dist\share\doc without path information
-    for dirpath, dirnames, filenames in os.walk(os.path.join("dist", "share",
-                                                             "doc")):
-        for f in filenames:
-            z.write(os.path.join(dirpath, f), f) # Forget about path
+    # Doc files (list taken from setup.py)
+    for f in ['CHANGELOG', 'COPYING', 'README.md', 'FAQ-body.html']:
+        z.write(f, f)
