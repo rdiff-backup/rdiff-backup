8ffd19ee66086973e711e4f026ba21dd1e57e48d
Arrigo Marchiori <ardovm@yahoo.it>
2019-12-15 17:37:48 +0100

    Include Windows in Travis CI build matrix (#212)
    * Use pyvenv-win
    If this works, credit goes to user @kirankotari on Travis CI forum:
    https://travis-ci.community/t/test-python-cli-script-against-all-python-versions/3350/2
    * Use GNU make from Chocolatey
    * Also rehash pyenv environment after installing modules
    * Build and link librsync statically
    * Run setup.py build explicitly
    * Also try to circumvent pyenv systematic error at installation

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ from distutils import log
 
 lflags_arg = []
 libname = ["rsync"]
+librsync_macros = []
 incdir_list = libdir_list = None
 
 if os.name == "posix" or os.name == "nt":
@@ -43,6 +44,10 @@ if os.name == "posix" or os.name == "nt":
             libdir_list = [os.path.join(LIBRSYNC_DIR, "lib")]
         if "-lrsync" in LIBS:
             libname = []
+
+if os.name == "nt":
+    # We rely on statically linked librsync
+    librsync_macros = [("rsync_EXPORTS", None)]
 
 
 # --- extend the build command to do templating of files ---
@@ -178,6 +183,7 @@ setup(
         Extension(
             "rdiff_backup._librsync",
             ["src/_librsyncmodule.c"],
+            define_macros=librsync_macros,
             include_dirs=incdir_list,
             library_dirs=libdir_list,
             libraries=libname,
