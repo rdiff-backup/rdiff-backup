d87f7be9ed906878a58eba52768f9d674158fc12
Joe Steele <joe@madewell.net>
2020-01-12 20:44:20 -0500

    Fix the restoration of hardlinked files (#239)
    This patch was originally submitted about 10 years ago.  Details can be
    found here: https://savannah.nongnu.org/bugs/?26848
    
    The patch has been by in use for some time, e.g., Debian:
    https://salsa.debian.org/python-team/applications/rdiff-backup/blob/7cd963df/debian/patches/03_fix_hardlinks.diff
    
    The hardlink tests have also been revised so as to agree with the changes in
    the patch.

--- src/rdiff_backup/Hardlink.py
+++ src/rdiff_backup/Hardlink.py
@@ -105,7 +105,14 @@ def rorp_eq(src_rorp, dest_rorp):
             or src_rorp.getnumlinks() == dest_rorp.getnumlinks() == 1):
         return 1  # Hard links don't apply
 
-    if src_rorp.getnumlinks() < dest_rorp.getnumlinks():
+    """The sha1 of linked files is only stored in the metadata of the first
+    linked file on the dest side.  If the first linked file on the src side is
+    deleted, then the sha1 will also be deleted on the dest side, so we test for this
+    & report not equal so that another sha1 will be stored with the next linked
+    file on the dest side"""
+    if (not islinked(src_rorp) and not dest_rorp.has_sha1()):
+        return 0
+    if src_rorp.getnumlinks() != dest_rorp.getnumlinks():
         return 0
     src_key = get_inode_key(src_rorp)
     index, remaining, dest_key, digest = _inode_index[src_key]
