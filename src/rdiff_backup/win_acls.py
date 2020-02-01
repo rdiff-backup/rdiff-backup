1c2741c20da9f5bc45387be7566e58c657547846
Eric L <ewl+git@lavar.de>
2020-02-01 13:25:27 +0100

    Fix some more ugly bytes output in strings using _safe_str
    FIX: remove some more ugly bytes output in strings using _safe_str, closes #238

--- src/rdiff_backup/win_acls.py
+++ src/rdiff_backup/win_acls.py
@@ -230,10 +230,18 @@ class ACL:
         return os.fsdecode(self.__bytes__())
 
     def from_string(self, acl_str):
+
+        def _safe_str(cmd):
+            """Transform bytes into string without risk of conversion error"""
+            if isinstance(cmd, str):
+                return cmd
+            else:
+                return str(cmd, errors='replace')
+
         lines = acl_str.splitlines()
         if len(lines) != 2 or not lines[0][:8] == b"# file: ":
             raise metadata.ParsingError(
-                "Bad record beginning: %a" % lines[0][:8])
+                "Bad record beginning: %s" % _safe_str(lines[0][:8]))
         filename = lines[0][8:]
         if filename == b'.':
             self.index = ()
