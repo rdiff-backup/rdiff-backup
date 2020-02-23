2e45babeca0bb7a76fe5d122aeabd49078cf32c0
Eric L <android@lavar.de>
2020-02-23 08:15:18 +0100

    Prepare beta release v1.9.1b0

--- tools/get_changelog_since.sh
+++ tools/get_changelog_since.sh
@@ -9,10 +9,17 @@ then
 fi
 RELTAG="${1}"
 
-echo "=== Changes ==="
-git log ${RELTAG}.. |
-	sed -n '/^ *[A-Z][A-Z][A-Z]: / s/^ *//p'
+echo "(make sure the version is the next correct one)"
+echo
+echo "New in v$($(dirname $0)/../setup.py --version) ($(date -I))"
+echo "----------------------------"
 
-echo "=== Authors ==="
+echo -e "\n## Changes\n"
 git log ${RELTAG}.. |
-	awk -F': *| *<' '$1 == "Author" { print $2 }' | sort -u
+	sed -n '/^ *[A-Z][A-Z][A-Z]: / s/^ */* /p'
+
+echo -e "\n## Authors\n"
+git log ${RELTAG}.. |
+	awk -F': *| *<' '$1 == "Author" { print "* " $2 }' | sort -u
+
+echo
