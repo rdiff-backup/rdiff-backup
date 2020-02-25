fca0c931edf15d5d4cf35c0d30b3397d24152d15
Patrik Dufresne <info@patrikdufresne.com>
2020-02-25 09:08:01 -0500

    Create a new rdiff-backup-delete script
    Replace the rdiff-backup-delete.py script and rmv-from-backup s ript by
    providing a complete implementation that support all the edge cases.
    Also include testcases.

---
+++ testing/rdiffbackupdeletetest.py
@@ -0,0 +1,145 @@
+import os
+import sys
+
+from distutils import spawn
+import subprocess
+import unittest
+
+PY2 = sys.version_info < (3,)
+PY3 = sys.version_info > (3,)
+
+# Working directories
+old_test_dir = os.path.join(os.path.dirname(os.getcwd()), 'rdiff-backup_testfiles')
+
+
+# Lookup for rdiff-backup-delete location.
+def rdiff_backup_delete(backup_dir=None, to_delete=None, extra_args=[], expected_ret_val=0, expected_output=None):
+    bin = spawn.find_executable(u"rdiff-backup-delete")
+    assert bin, "can't find rdiff-backup-delete"
+    cmdargs = [bin.encode('utf8')]
+    if backup_dir:
+        cmdargs.append(b'--rdiff-backup-dir=' + backup_dir)
+    if to_delete:
+        cmdargs.append(to_delete)
+    if extra_args:
+        cmdargs.extend(extra_args)
+    cmdline = b" ".join(cmdargs)
+    print("Executing: %r" % (cmdline,))
+    p = subprocess.Popen(cmdargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = p.communicate()
+    ret_val = p.poll()
+    if expected_ret_val is not None:
+        assert ret_val == expected_ret_val, "Return code %d of command `%s` doesn't matches expected value: %d.\n%s\n%s" % \
+            (ret_val, cmdline, expected_ret_val, output, error)
+    if expected_output is not None:
+        assert expected_output in output or expected_output in error, "Output %s %s of command `%s` doesn't matches expected output:\n`%s`" % \
+            (output, error, cmdline, expected_output)
+
+
+def rdiff_backup_verify(backup_dir):
+    assert backup_dir
+    assert os.path.isdir(backup_dir)
+
+    # Lookup rdiff-backup executable.
+    bin = spawn.find_executable("rdiff-backup")
+    assert bin, "can't find rdiff-backup"
+
+    # Build the command line
+    cmdargs = [bin.encode('utf8'), b'--verify', backup_dir]
+    cmdline = b" ".join(cmdargs)
+    print("Executing: %r" % (cmdline,))
+    DEVNULL = open(os.devnull, 'wb')
+    try:
+        p = subprocess.Popen(cmdargs, stdout=DEVNULL, stderr=DEVNULL)
+        assert p.wait() == 0, 'rdiff-backup verify failed'
+    finally:
+        DEVNULL.close()
+
+
+class RdiffBackupDeleteTest(unittest.TestCase):
+
+    repo = ""
+
+    def _copy_repo(self, reponame):
+        # Copy the required repo to a temporary location.
+        # We need to use os command line to properly copy and delete special files.
+        self.repo = b"/tmp/deletetest"
+        if os.path.exists(self.repo):
+            subprocess.check_call([b'rm', b'-Rf', self.repo])
+        src = os.path.join(old_test_dir.encode('utf8'), reponame)
+        subprocess.check_call([b'cp', b'-R', src, self.repo])
+
+    def tearDown(self):
+        if os.path.exists(self.repo):
+            subprocess.check_call(['rm', '-Rf', self.repo])
+
+    def test_arguments(self):
+        # Call with --help or -h should return 0 and print the usage.
+        rdiff_backup_delete(extra_args=[b'--help'],
+                            expected_output=b'Usage:')
+        rdiff_backup_delete(extra_args=[b'-h'],
+                            expected_output=b'Usage:')
+        # Call without arguments
+        rdiff_backup_delete(extra_args=[], expected_ret_val=1,
+                            expected_output=b'fatal: missing arguments')
+        # Call with invalid arguments
+        rdiff_backup_delete(extra_args=[b'--invalid'], expected_ret_val=1,
+                            expected_output=b'fatal: bad command line: option --invalid not recognized')
+
+    def test_rdiff_backup_dir(self):
+        # without rdiff-backup-dir
+        rdiff_backup_delete(to_delete=b'somefile', expected_ret_val=1,
+                            expected_output=b'fatal: not a rdiff-backup repository (or any parent up to mount point /)')
+        # with invalid rdiff-backup-dir
+        rdiff_backup_delete(backup_dir=b'coucou', to_delete=b'somefile',
+                            expected_ret_val=1,
+                            expected_output=b"fatal: can't find rdiff-backup-data folder in directory: coucou")
+
+    def test_delete_with_file(self):
+        self._copy_repo(b'restoretest4')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'tmp/changed')
+        rdiff_backup_verify(self.repo)
+
+    def test_delete_with_directory(self):
+        self._copy_repo(b'restoretest4')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'tmp')
+        rdiff_backup_verify(self.repo)
+
+    def test_delete_with_deleted_directory(self):
+        self._copy_repo(b'restoretest3')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'increment1')
+        rdiff_backup_verify(self.repo)
+
+    def test_delete_with_symlink(self):
+        self._copy_repo(b'restoretest3')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'various_file_types/symbolic_link')
+        rdiff_backup_verify(self.repo)
+
+    def test_delete_with_hardlink(self):
+        self._copy_repo(b'restoretest3')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'various_file_types/two_hardlinked_files1')
+        rdiff_backup_verify(self.repo)
+
+    def test_delete_with_fifo(self):
+        self._copy_repo(b'restoretest5')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'fifo')
+        rdiff_backup_verify(self.repo)
+
+    def test_delete_with_non_utf8(self):
+        self._copy_repo(b'restoretest5')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'various_file_types/\xd8\xab\xb1Wb\xae\xc5]\x8a\xbb\x15v*\xf4\x0f!\xf9>\xe2Y\x86\xbb\xab\xdbp\xb0\x84\x13k\x1d\xc2\xf1\xf5e\xa5U\x82\x9aUV\xa0\xf4\xdf4\xba\xfdX\x03\x82\x07s\xce\x9e\x8b\xb34\x04\x9f\x17 \xf4\x8f\xa6\xfa\x97\xab\xd8\xac\xda\x85\xdcKvC\xfa#\x94\x92\x9e\xc9\xb7\xc3_\x0f\x84g\x9aB\x11<=^\xdbM\x13\x96c\x8b\xa7|*"\\\'^$@#!(){}?+ ~` ')
+        rdiff_backup_verify(self.repo)
+
+    def test_delete_access_control_lists(self):
+        self._copy_repo(b'restoretest3')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'increment1')
+        rdiff_backup_verify(self.repo)
+
+    def test_extended_attributes(self):
+        self._copy_repo(b'restoretest3')
+        rdiff_backup_delete(backup_dir=self.repo, to_delete=b'newdir')
+        rdiff_backup_verify(self.repo)
+
+
+if __name__ == "__main__":
+    unittest.main()
