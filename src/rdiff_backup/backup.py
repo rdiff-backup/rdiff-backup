f443ca4ce1ae910fa333211f300350a0c12ca143
zjw <joe@madewell.net>
2020-03-08 03:35:52 -0400

    Fix "UpdateError: Updated mirror temp file does not match source" (#241)
    FIX: UpdateError: Updated mirror temp file does not match source, Closes #237
    
    The error is known to occur when hardlinked files are backed up between systems
    that save selinux security contexts in different ways.  This patch was
    originally submitted 10+ years ago.  Details here:
    
    https://savannah.nongnu.org/bugs/?26796
    
    The patch has been included with Fedora:
    
    https://src.fedoraproject.org/rpms/rdiff-backup/blob/b2280a15e5b8c76696364b1e43b10ad14b828f09/f/rdiff-backup-1.2.8-hardlinks.patch

--- src/rdiff_backup/backup.py
+++ src/rdiff_backup/backup.py
@@ -616,8 +616,24 @@ class PatchITRB(rorpiter.ITRBranch):
                 return 1  # SpecialFile
         elif not self.patch_diff_to_temp(basis_rp, diff_rorp, new):
             return 0
-        if new.lstat() and not diff_rorp.isflaglinked():
-            rpath.copy_attribs(diff_rorp, new)
+        if new.lstat():
+            if diff_rorp.isflaglinked():
+                if Globals.eas_write:
+                    """ `isflaglinked() == True` implies that we are processing
+                    the 2nd (or later) file in a group of files linked to an
+                    inode.  As such, we don't need to perform the usual
+                    `copy_attribs(diff_rorp, new)` for the inode because that
+                    was already done when the 1st file in the group was
+                    processed.  Nonetheless, we still must perform the following
+                    task (which would have normally been performed by
+                    `copy_attribs()`).  Otherwise, the subsequent call to
+                    `matches_cached_rorp(diff_rorp, new)` will fail because the
+                    new rorp's metadata would be missing the extended attribute
+                    data.
+                    """
+                    new.data['ea'] = diff_rorp.get_ea()
+            else:
+                rpath.copy_attribs(diff_rorp, new)
         return self.matches_cached_rorp(diff_rorp, new)
 
     def patch_hardlink_to_temp(self, diff_rorp, new):
