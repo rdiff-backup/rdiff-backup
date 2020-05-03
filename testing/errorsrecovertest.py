8e70069b2be48df7865f31fc6bc56623b93e2460
Eric L <ericzolf@users.noreply.github.com>
2020-05-03 07:52:47 +0200

    Improve the PR #328 based on feedback from zjw (#332)
    - re-write the tests to take into consideration that --remove-older-than doesn't need --allow-duplicate-timestamps
    - improve the error message and the man-page also accordingly
    - make the tests also slightly more robust, sorting entry and using date instead of number of backups

--- testing/errorsrecovertest.py
+++ testing/errorsrecovertest.py
@@ -36,8 +36,9 @@ class BrokenRepoTest(unittest.TestCase):
             rdiff_backup(1, 1, source_rp.get_path(), target_rp.get_path(),
                          current_time=suffix * 10000)
         # identify the oldest (aka first) mirror metadata snapshot
+        # and sort the list because some filesystems don't respect the order
         rb_data_rp = target_rp.append("rdiff-backup-data")
-        files_list = list(filter(
+        files_list = sorted(filter(
             lambda x: x.startswith(b"mirror_metadata."),
             rb_data_rp.listdir()))
         meta_snapshot_rp = rb_data_rp.append(files_list[8])
@@ -46,17 +47,24 @@ class BrokenRepoTest(unittest.TestCase):
             b".snapshot.gz", b".diff.gz"))
         rpath.copy(meta_snapshot_rp, meta_dupldiff_rp)
 
+        # this succeeds
+        rdiff_backup(1, 1, target_rp.get_path(), None,
+                     extra_options=b"--check-destination-dir")
         # now this should fail
         source_rp.append("file15").touch()
         rdiff_backup(1, 1, source_rp.get_path(), target_rp.get_path(),
                      current_time=15 * 10000, expected_ret_val=1)
+        # and this should also fail
+        rdiff_backup(1, 1, target_rp.get_path(), None, expected_ret_val=1,
+                     extra_options=b"--check-destination-dir")
         # but this should succeed
-        rdiff_backup(1, 1, source_rp.get_path(), target_rp.get_path(),
-                     current_time=15 * 10000,
-                     extra_options=b"--allow-duplicate-timestamps")
-        # now we can clean-up
         rdiff_backup(1, 1, target_rp.get_path(), None,
-                     extra_options=b"--allow-duplicate-timestamps --remove-older-than 5B --force")
+                     extra_options=b"--allow-duplicate-timestamps --check-destination-dir")
+        # now we can clean-up, getting rid of the duplicate metadata mirrors
+        # NOTE: we could have cleaned-up even without checking/fixing the directory
+        #       but this shouldn't be the recommended practice.
+        rdiff_backup(1, 1, target_rp.get_path(), None,
+                     extra_options=b"--remove-older-than 100000 --force")
         # and this should at last succeed
         source_rp.append("file16").touch()
         rdiff_backup(1, 1, source_rp.get_path(), target_rp.get_path(),
