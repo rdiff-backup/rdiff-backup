7150d44a557693f5388167d74cb6ecdf414f2e83
Eric L <ericzolf@users.noreply.github.com>
2020-07-03 07:38:30 +0200

    handle better exfat strangeness (#412)
    CHG: explicitly refuse to back-up to exFAT because it doesn't handle properly case insensitive deletion of files, closes #38

--- src/rdiff_backup/fs_abilities.py
+++ src/rdiff_backup/fs_abilities.py
@@ -322,7 +322,14 @@ class FSAbilities:
         if lower_a.lstat():
             lower_a.delete()
             upper_a.setdata()
-            assert not upper_a.lstat()
+            if upper_a.lstat():
+                # we know that (fuse-)exFAT 1.3.0 takes 1sec to register the
+                # deletion (July 2020)
+                log.Log.FatalError(
+                    "We're sorry but the target file system at '%s' isn't "
+                    "deemed reliable enough for a backup. It takes too long "
+                    "or doesn't register case insensitive deletion of files."
+                    % subdir.get_safepath())
             self.case_sensitive = 0
         else:
             upper_a.delete()
