643a5d327c57fedc359acf4d60b9e9a84690c864
Eric L <ewl+git@lavar.de>
2020-12-30 20:42:23 +0100

    Sort arguments by type and make actions mutually exclusive

--- src/rdiffbackup/arguments.py
+++ src/rdiffbackup/arguments.py
@@ -72,42 +72,118 @@ class SelectAction(argparse.Action):
 
 
 def _parse_old(args):
+    """
+    Parse arguments according to old parameters of rdiff-backup.
+    The hint in square brackets at the beginning of the help are in preparation
+    for the new way of parsing:
+        rdiff-backup <opt(ions)> <act(ion)> <sub(-options)> <paths>
+    Note that actions are mutually exclusive and that '[act=]' will need to be
+    split into an action and a sub-option.
+    """
 
     parser = argparse.ArgumentParser(
         description="local/remote mirror and incremental backup")
 
-    parser.add_argument("--allow-duplicate-timestamps", action="store_true",
-        help="[sub] ignore duplicate metadata while checking destination dir")
     parser.add_argument("--api-version", type=int,
         help="[opt] integer to set the API version forcefully used")
-    parser.add_argument("-b", "--backup-mode",
-        dest="action", action="store_const", const="backup",
-        help="[act] back-up directory into back-up repository")
-    parser.add_argument("--calculate-average",
-        dest="action", action="store_const", const="calculate-average",
-        help="[act] calculate average across multiple statistic files")
-    parser.add_argument("--check-destination-dir",
-        dest="action", action="store_const", const="check-destination-dir",
-        help="[act] check-destination-dir")
-    parser.add_argument("--compare",
-        dest="action", action="store_const", const="compare",
-        help="[act] compare normal (at time now)")
-    parser.add_argument("--compare-at-time", type=str, metavar="AT_TIME",
-        help="[act=] compare normal at given time")
-    parser.add_argument("--compare-hash",
-        dest="action", action="store_const", const="compare-hash",
-        help="[act] compare by hash (at time now)")
-    parser.add_argument("--compare-hash-at-time", type=str, metavar="AT_TIME",
-        help="[act=] compare by hash at given time")
-    parser.add_argument("--compare-full",
-        dest="action", action="store_const", const="compare-full",
-        help="[act] compare full (at time now)")
-    parser.add_argument("--compare-full-at-time", type=str, metavar="AT_TIME",
-        help="[act=] compare full at given time")
-    parser.add_argument("--create-full-path", action="store_true",
-        help="[sub] create full necessary path to backup repository")
     parser.add_argument("--current-time", type=int,
         help="[opt] fake the current time in seconds (for testing)")
+    parser.add_argument("--force", action="store_true",
+        help="[opt] force action")
+    parser.add_argument("--fsync", default=True,
+        action=argparse.BooleanOptionalAction,
+        help="[opt] do (or not) often sync the file system (_not_ doing it is faster but can be dangerous)")
+    parser.add_argument("--null-separator", action="store_true",
+        help="[opt] use null instead of newline in input and output files")
+    parser.add_argument("--old", action="store_true",
+        help="[opt] enforce the usage of the old parameters")
+    parser.add_argument("--override-chars-to-quote", type=str,
+        metavar="CHARS_TO_QUOTE",
+        help="[opt] string of characters to quote for safe storing")
+    parser.add_argument("--parsable-output", action="store_true",
+        help="[opt] output in computer parsable format")
+    parser.add_argument("--remote-schema", "--remote-cmd", type=str,
+        help="[opt] alternative command to call remotely rdiff-backup")
+    parser.add_argument("--remote-tempdir", type=str, metavar="DIR_PATH",
+        help="[opt] use path as temporary directory on the remote side")
+    parser.add_argument("--restrict", type=str, metavar="DIR_PATH",
+        help="[opt] restrict remote access to given path")
+    parser.add_argument("--restrict-read-only", type=str, metavar="DIR_PATH",
+        help="[opt] restrict remote access to given path, in read-only mode")
+    parser.add_argument("--restrict-update-only", type=str, metavar="DIR_PATH",
+        help="[opt] restrict remote access to given path, in backup update mode")
+    parser.add_argument("--ssh-no-compression", action="store_true",
+        help="[opt] use SSH without compression with default remote-schema")
+    parser.add_argument("--tempdir", type=str, metavar="DIR_PATH",
+        help="[opt] use given path as temporary directory")
+    parser.add_argument("--terminal-verbosity", type=int,
+        choices=range(0,10),
+        help="[opt] verbosity on the terminal, default given by --verbosity")
+    parser.add_argument("-v", "--verbosity", type=int,
+        choices=range(0,10), default=3,
+        help="[opt] overall verbosity on terminal and in logfiles (default is 3)")
+
+    action_group = parser.add_mutually_exclusive_group()
+    action_group.add_argument(
+        "-b", "--backup-mode",
+        dest="action", action="store_const", const="backup",
+        help="[act] back-up directory into back-up repository")
+    action_group.add_argument("--calculate-average",
+        dest="action", action="store_const", const="calculate-average",
+        help="[act] calculate average across multiple statistic files")
+    action_group.add_argument("--check-destination-dir",
+        dest="action", action="store_const", const="check-destination-dir",
+        help="[act] check-destination-dir")
+    action_group.add_argument("--compare",
+        dest="action", action="store_const", const="compare",
+        help="[act] compare normal (at time now)")
+    action_group.add_argument("--compare-at-time", type=str, metavar="AT_TIME",
+        help="[act=] compare normal at given time")
+    action_group.add_argument("--compare-hash",
+        dest="action", action="store_const", const="compare-hash",
+        help="[act] compare by hash (at time now)")
+    action_group.add_argument("--compare-hash-at-time",
+        type=str, metavar="AT_TIME",
+        help="[act=] compare by hash at given time")
+    action_group.add_argument("--compare-full",
+        dest="action", action="store_const", const="compare-full",
+        help="[act] compare full (at time now)")
+    action_group.add_argument(
+        "--compare-full-at-time", type=str, metavar="AT_TIME",
+        help="[act=] compare full at given time")
+    action_group.add_argument("--list-at-time", type=str, metavar="AT_TIME",
+        help="[act=] list files and directories at given time")
+    action_group.add_argument("--list-changed-since", type=str, metavar="AT_TIME",
+        help="[act=] list changed files and directories since given time")
+    action_group.add_argument("-l", "--list-increments",
+        dest="action", action="store_const", const="list-increments",
+        help="[act] list increments in backup repository")
+    action_group.add_argument("--list-increment-sizes",
+        dest="action", action="store_const", const="list-increment-sizes",
+        help="[act] list increments and their size in backup repository")
+    action_group.add_argument("--remove-older-than", type=str, metavar="AT_TIME",
+        help="[act] remove increments older than given time")
+    action_group.add_argument("-r", "--restore-as-of", type=str, metavar="AT_TIME",
+        help="[act] restore files from repo as of given time")
+    action_group.add_argument("-s", "--server",
+        dest="action", action="store_const", const="server",
+        help="[act] start rdiff-backup in server mode")
+    action_group.add_argument("--test-server",
+        dest="action", action="store_const", const="test-server",
+        help="[act] test communication to multiple remote servers")
+    action_group.add_argument("--verify",
+        dest="action", action="store_const", const="verify",
+        help="[act] verify hash values in backup repo (at time now)")
+    action_group.add_argument("--verify-at-time", type=str, metavar="AT_TIME",
+        help="[act] verify hash values in backup repo (at given time)")
+    action_group.add_argument("-V", "--version",
+        dest="action", action="store_const", const="version",
+        help="[act] output the rdiff-backup version and exit")
+
+    parser.add_argument("--allow-duplicate-timestamps", action="store_true",
+        help="[sub] ignore duplicate metadata while checking destination dir")
+    parser.add_argument("--create-full-path", action="store_true",
+        help="[sub] create full necessary path to backup repository")
     parser.add_argument("--SELECT", action=SelectAction, metavar="GLOB",
         help="[sub] SELECT files according to glob pattern")
     parser.add_argument("--SELECT-device-files",
@@ -134,7 +210,6 @@ def _parse_old(args):
     parser.add_argument("--SELECT-globbing-filelist-stdin",
         action=SelectAction, type=bool,
         help="[sub] SELECT files according to glob list from standard input")
-    #parser.add_argument("--SELECT-mirror", type=str)  # obsolete?
     parser.add_argument("--SELECT-other-filesystems",
         action=SelectAction, type=bool, default=True,
         help="[sub] SELECT files from other file systems than the source one")
@@ -147,20 +222,8 @@ def _parse_old(args):
     parser.add_argument("--SELECT-special-files",
         action=SelectAction, type=bool, default=True,
         help="[sub] SELECT all device, fifo, socket files, and symbolic links")
-    parser.add_argument("--force", action="store_true",
-        help="[opt] force action")
-    parser.add_argument("--group-mapping-file", type=str, metavar="FILENAME",
+    parser.add_argument("--group-mapping-file", type=str, metavar="MAP_FILE",
         help="[sub] map groups according to file")
-    parser.add_argument("--list-at-time", type=str,
-        help="[act=] list files and directories at given time")
-    parser.add_argument("--list-changed-since", type=str,
-        help="[act=] list changed files and directories since given time")
-    parser.add_argument("-l", "--list-increments",
-        dest="action", action="store_const", const="list-increments",
-        help="[act] list increments in backup repository")
-    parser.add_argument("--list-increment-sizes",
-        dest="action", action="store_const", const="list-increment-sizes",
-        help="[act] list increments and their size in backup repository")
     parser.add_argument("--never-drop-acls", action="store_true",
         help="[sub] exit with error instead of dropping acls or acl entries.")
     parser.add_argument("--max-file-size",
@@ -171,10 +234,10 @@ def _parse_old(args):
         help="[sub] exclude files smaller than given size in bytes")
     parser.add_argument("--acls", default=True,
         action=argparse.BooleanOptionalAction,
-        help="[sub] backup (or not) Access Control Lists")
+        help="[sub] handle (or not) Access Control Lists")
     parser.add_argument("--carbonfile", default=True,
         action=argparse.BooleanOptionalAction,
-        help="[sub] backup (or not) carbon files on MacOS X")
+        help="[sub] handle (or not) carbon files on MacOS X")
     parser.add_argument("--compare-inode", default=True,
         action=argparse.BooleanOptionalAction,
         help="[sub] compare (or not) inodes to decide if hard-linked files have changed")
@@ -190,55 +253,23 @@ def _parse_old(args):
                   b")$"),
         help="[sub] regexp to select files not being compressed")
     parser.add_argument("--eas", default=True,
-        action=argparse.BooleanOptionalAction)
+        action=argparse.BooleanOptionalAction,
+        help="[sub] handle (or not) Extended Attributes")
     parser.add_argument("--file-statistics", default=True,
-        action=argparse.BooleanOptionalAction)
-    parser.add_argument("--fsync", default=True,
-        action=argparse.BooleanOptionalAction)
+        action=argparse.BooleanOptionalAction,
+        help="[sub] do (or not) generate statistics file during backup")
     parser.add_argument("--hard-links", default=True,
-        action=argparse.BooleanOptionalAction)
-    parser.add_argument("--null-separator", action="store_true")
-    parser.add_argument("--old", action="store_true",
-        help="[opt] enforce the usage of the old parameters")
-    parser.add_argument("--override-chars-to-quote", type=str)
-    parser.add_argument("--parsable-output", action="store_true")
-    parser.add_argument("--preserve-numerical-ids", action="store_true")
-    parser.add_argument("--print-statistics", action="store_true")
-    parser.add_argument("--remote-cmd", dest="remote_schema", type=str)
-    parser.add_argument("--remote-schema", type=str)
-    parser.add_argument("--remote-tempdir", type=str)
-    parser.add_argument("--remove-older-than", type=str, metavar="AT_TIME",
-        help="[action] remove increments older than given time")
-    parser.add_argument("-r", "--restore-as-of", type=str, metavar="AT_TIME",
-        help="[action] restore files from repo as of given time")
-    parser.add_argument("--restrict", type=str)
-    parser.add_argument("--restrict-read-only", type=str)
-    parser.add_argument("--restrict-update-only", type=str)
-    parser.add_argument("-s", "--server",
-        dest="action", action="store_const", const="server",
-        help="[action] start rdiff-backup in server mode")
-    parser.add_argument("--ssh-no-compression", action="store_true")
-    parser.add_argument("--tempdir", type=str)
-    parser.add_argument("--terminal-verbosity", type=int,
-        choices=range(0,10),
-        help="[opt] verbosity on the terminal, default given by --verbosity")
-    parser.add_argument("--test-server",
-        dest="action", action="store_const", const="test-server",
-        help="[action] test communication to multiple remote servers")
-    parser.add_argument("--use-compatible-timestamps", action="store_true")
-    parser.add_argument("--user-mapping-file", type=str, metavar="FILENAME",
+        action=argparse.BooleanOptionalAction,
+        help="[sub] preserve (or not) hard links.")
+    parser.add_argument("--preserve-numerical-ids", action="store_true",
+        help="[sub] preserve user and group IDs instead of names")
+    parser.add_argument("--print-statistics", action="store_true",
+        help="[sub] print statistics after a successful backup")
+    parser.add_argument("--use-compatible-timestamps", action="store_true",
+        help="[sub] use hyphen instead of colon to represent time")
+    parser.add_argument("--user-mapping-file", type=str, metavar="MAP_FILE",
         help="[sub] map users according to file")
-    parser.add_argument("-v", "--verbosity", type=int,
-        choices=range(0,10), default=3,
-        help="[opt] overall verbosity on terminal and in logfiles (default is 3)")
-    parser.add_argument("--verify",
-        dest="action", action="store_const", const="verify",
-        help="[action] verify hash values in backup repo (at time now)")
-    parser.add_argument("--verify-at-time", type=str, metavar="AT_TIME",
-        help="[action] verify hash values in backup repo (at given time)")
-    parser.add_argument("-V", "--version",
-        dest="action", action="store_const", const="version",
-        help="[action] output the rdiff-backup version and exit")
+
     parser.add_argument("paths", nargs='*',
         help="[args] paths to be handled by the chosen action")
 
