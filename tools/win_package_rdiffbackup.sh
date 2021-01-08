24406e0383bfb5cbee219f01e30b7568918b29db
Eric L <ewl+git@lavar.de>
2021-01-08 22:22:59 +0100

    Add and adapt windows build scripts

---
+++ tools/win_package_rdiffbackup.sh
@@ -0,0 +1,36 @@
+BITS=$1
+PYDIRECT=$2
+
+if [[ ${BITS} == *32 ]] || [[ ${BITS} == *86 ]]
+then
+	bits=32
+	lib_win_bits=Win32
+	py_win_bits=win32
+elif [[ ${BITS} == *64 ]]
+then
+	bits=64
+	lib_win_bits=x64
+	py_win_bits=win-amd64
+else
+	echo "ERROR: bits size must be 32 or 64, not '${BITS}'." >&2
+	exit 1
+fi
+
+PYEXE=python.exe
+PYINST=PyInstaller.exe
+if [[ -n ${PYDIRECT} ]]
+then
+	py_dir=C:/Python${bits}
+	PYEXE=${py_dir}/${PYEXE}
+	PYINST=${py_dir}/Scripts/${PYINST}
+fi
+
+ver_name=rdiff-backup-$(${PYTHON} setup.py --version)
+
+cp CHANGELOG.md COPYING README.md \
+	docs/FAQ.md docs/examples.md docs/DEVELOP.md \
+	docs/Windows-README.md docs/Windows-DEVELOP.md \
+	build/${ver_name}-${bits}
+pushd build
+7z a -tzip ../dist/${ver_name}.${py_win_bits}exe.zip ${ver_name}-${bits}
+popd
