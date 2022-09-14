eddeb8f706550b56cf414cb21743a5f8aca0ca99
Eric L <ericzolf@users.noreply.github.com>
2022-09-14 13:06:22 +0200

    Fix missing modules in Windows binary (#732)
    * Pin version of PyInstaller to 5.1
    
    Addresses #731, see https://github.com/orgs/pyinstaller/discussions/7078
    
    DEV: use tox to call pyinstaller in a cleaner environment, addresses #731

--- tools/win_provision.sh
+++ tools/win_provision.sh
@@ -10,7 +10,8 @@ for bits in 32 64
 do
 	C:/Python${bits}/python.exe -VV
 	C:/Python${bits}/Scripts/pip.exe install --upgrade \
-		pywin32 pyinstaller wheel certifi setuptools-scm tox PyYAML
+		pywin32 pyinstaller==5.1 wheel certifi setuptools-scm tox PyYAML
+# FIXME PyInstaller 5.2 and 5.3 fail to grab all modules
 	C:/Python${bits}/python.exe -c \
 		'import pywintypes, winnt, win32api, win32security, win32file, win32con'
 done
