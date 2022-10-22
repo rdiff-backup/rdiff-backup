ab9c993c123d4b1fa024a249abafe8f988e03164
Eric L <ewl+git@lavar.de>
2022-10-22 22:45:10 +0200

    Preparing release v2.1.3b0
    Hence the many different commits as I developed the script

--- tools/make_release.sh
+++ tools/make_release.sh
@@ -113,7 +113,8 @@ fi
 
 set -e  # exit immediately if something goes wrong
 
-gh pr review ${PR_NUMBER} --approve --body "Auto-approve by $(basename $0)"
+gh pr review ${PR_NUMBER} --comment \
+	--body "Automatically created and merged by $(basename $0)"
 
 gh pr merge ${PR_NUMBER} --auto --squash --delete-branch
 
