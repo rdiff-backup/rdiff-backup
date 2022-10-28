853ee5aa12880f66ca43745f55a15f4ae0f60d28
Eric L <ericzolf@users.noreply.github.com>
2022-10-28 14:34:42 +0200

    Add quoting test (#769)
    Closes #275

--- testing/action_backuprestore_test.py
+++ testing/action_backuprestore_test.py
@@ -96,7 +96,7 @@ class ActionBackupRestoreTest(unittest.TestCase):
         self.assertEqual(comtst.rdiff_backup_action(
             False, False, self.from1_path, self.bak_path,
             ("--api-version", "201", "--current-time", "10000",
-             "--chars-to-quote", "A", "--use-compatible-timestamps"),
+             "--chars-to-quote", "A:"),  # colon for Windows compatibility
             b"backup", ()), 0)
 
         # then we restore once the full repo, once a sub-path
@@ -108,6 +108,76 @@ class ActionBackupRestoreTest(unittest.TestCase):
             True, True, self.bak_path, self.to2_path,
             ("--api-version", "201"),
             b"restore", ()), 0)
+
+    def tearDown(self):
+        # we clean-up only if the test was successful
+        if self.success:
+            fileset.remove_fileset(self.base_dir, self.from1_struct)
+            fileset.remove_fileset(self.base_dir, self.from2_struct)
+            fileset.remove_fileset(self.base_dir, {"bak": {"type": "dir"}})
+            fileset.remove_fileset(self.base_dir, {"to1": {"type": "dir"}})
+            fileset.remove_fileset(self.base_dir, {"to2": {"type": "dir"}})
+
+
+class PreQuotingTest(unittest.TestCase):
+    """
+    Test rdiff-backup's quoting mechanisms cf. #275
+    """
+
+    def setUp(self):
+        self.base_dir = os.path.join(comtst.abs_test_dir,
+                                     b"action_backuprestore")
+        self.from1_struct = {
+            "from1": {"subs": {  # CODE.txt pre-quoted
+                ";067;079;068;069.txt": {"content": "initial"},
+            }}
+        }
+        self.from1_path = os.path.join(self.base_dir, b"from1")
+        self.from2_struct = {
+            "from2": {"subs": {  # CODE.txt pre-quoted and non-quoted
+                ";067;079;068;069.txt": {"content": "modified"},
+                "CODE.txt": {"content": "whatever"},
+            }}
+        }
+        self.from2_path = os.path.join(self.base_dir, b"from2")
+        fileset.create_fileset(self.base_dir, self.from1_struct)
+        fileset.create_fileset(self.base_dir, self.from2_struct)
+        fileset.remove_fileset(self.base_dir, {"bak": {"type": "dir"}})
+        fileset.remove_fileset(self.base_dir, {"to1": {"type": "dir"}})
+        fileset.remove_fileset(self.base_dir, {"to2": {"type": "dir"}})
+        self.bak_path = os.path.join(self.base_dir, b"bak")
+        self.to1_path = os.path.join(self.base_dir, b"to1")
+        self.to2_path = os.path.join(self.base_dir, b"to2")
+        self.success = False
+
+    def test_pre_quoted_files(self):
+        # we backup using a specific chars-to-quote
+        self.assertEqual(comtst.rdiff_backup_action(
+            False, False, self.from1_path, self.bak_path,
+            ("--api-version", "201", "--current-time", "10000",
+             "--chars-to-quote", "A-Z:"),  # colon for Windows compatibility
+            b"backup", ()), 0)
+        self.assertEqual(comtst.rdiff_backup_action(
+            True, True, self.from2_path, self.bak_path,
+            ("--api-version", "201", "--current-time", "20000",
+             "--chars-to-quote", "A-Z:"),  # colon for Windows compatibility
+            b"backup", ()), 0)
+
+        # then we restore once the full repo, once a sub-path
+        self.assertEqual(comtst.rdiff_backup_action(
+            True, False, self.bak_path, self.to1_path,
+            ("--api-version", "201"),
+            b"restore", ("--at", "10000")), 0)
+        self.assertEqual(comtst.rdiff_backup_action(
+            True, True, self.bak_path, self.to2_path,
+            ("--api-version", "201"),
+            b"restore", ("--at", "20000")), 0)
+        # use sets to avoid issues with directory ordering
+        self.assertEqual(set(os.listdir(self.to1_path)),
+                         {b';067;079;068;069.txt'})
+        self.assertEqual(set(os.listdir(self.to2_path)),
+                         {b';067;079;068;069.txt', b'CODE.txt'})
+        self.success = True
 
     def tearDown(self):
         # we clean-up only if the test was successful
