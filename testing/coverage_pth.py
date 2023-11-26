01a04cdc4e62737e3791827856c0d8c926a1a809
Eric L. <ewl+git@lavar.de>
2023-11-26 15:33:04 +0100

    Make sure tox.ini doesn't write if called without venv
    NEW: tox.ini supports --current-env as normal user, avoiding to write in root-protected paths, closes #949
    
    Added a script coverage_pth.py to write the file coverage.pth in the
    right place depending on available access rights.

---
+++ testing/coverage_pth.py
@@ -0,0 +1,31 @@
+# write the hook file which will make sure that coverage is loaded
+# also for sub-processes, like for "client/server" rdiff-backup
+
+import os
+import site
+import sys
+
+for site_path in sys.path:
+    coverage_pth = os.path.join(site_path, "coverage.pth")
+    if os.path.isfile(coverage_pth):
+        print("The file {} exists already".format(coverage_pth))
+        sys.exit(0)
+
+if site.ENABLE_USER_SITE:
+    site_packages = [site.getusersitepackages()]
+else:  # we're probably in a virtualenv
+    site_packages = site.getsitepackages()
+
+for site_path in site_packages:
+    coverage_pth = os.path.join(site_path, "coverage.pth")
+    try:
+        os.makedirs(site_path, exist_ok=True)
+        with open(coverage_pth, "w") as fd:
+            fd.write("import coverage; coverage.process_startup()\n")
+            print("The file {} has been written".format(coverage_pth))
+            sys.exit(0)
+    except OSError as exc:
+        print("Couldn't write the file {} due to {}".format(coverage_pth, exc))
+
+print("The coverage.pth file could be written nowhere")
+sys.exit(1)
