73cb807b025fb19babda377a562474430d4be051
Eric L <ericzolf@users.noreply.github.com>
2023-12-23 11:01:27 +0100

    Save Python module versions under Windows (#961)
    NEW: Python module versions are saved as part of zip file for Windows using pip freeze, closes #957
    
    * Add get_pip_freeze.py script to compensate for missing output capability of pip freeze and tox' lack of shell to redirect the output

---
+++ tools/get_pip_freeze.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# because pip freeze doesn't offer a way to give an output file, and tox
+# doesn't have a shell for redirection, we need to write our own pip freeze
+# https://stackoverflow.com/questions/24321933/programmatically-generate-requirements-txt-file
+# Call with `./tools/get_pip_freeze.py <outputfile>`
+
+import sys
+import importlib.metadata as ilmd
+
+installed = {pkg for pkgs in ilmd.packages_distributions().values() for pkg in pkgs}
+req_str = "\n".join(f"{pkg}=={ilmd.version(pkg)}" for pkg in sorted(installed))
+
+with open(sys.argv[1], "w") as f:
+    f.write(req_str)
