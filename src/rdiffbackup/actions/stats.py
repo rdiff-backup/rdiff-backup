5d4813f2f692052088ca210d8750b785a36723a8
Eric L. <ewl+git@lavar.de>
2024-06-01 12:40:36 +0200

    Merge statistics command line into action
    CHG: rdiff-backup-statistics has been replaced by a 'stats' action to the rdiff-backup command, closes #772

---
+++ src/rdiffbackup/actions/stats.py
@@ -0,0 +1,127 @@
+# Copyright 2021 the rdiff-backup project
+#
+# This file is part of rdiff-backup.
+#
+# rdiff-backup is free software; you can redistribute it and/or modify
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# rdiff-backup is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with rdiff-backup; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA
+
+"""
+A built-in rdiff-backup action plug-in to display statistics of a repository.
+
+This plug-in shows different statistics about a specific backup repository.
+"""
+
+from rdiff_backup import Globals
+from rdiffbackup import actions
+from rdiffbackup.locations import repository
+
+
+class StatsAction(actions.BaseAction):
+    """
+    Verify that files in a backup repository correspond to their stored hash,
+    or that servers are properly reachable.
+    """
+
+    name = "stats"
+    security = "validate"
+
+    @classmethod
+    def add_action_subparser(cls, sub_handler):
+        subparser = super().add_action_subparser(sub_handler)
+        subparser.add_argument(
+            "--from",
+            metavar="BEGIN_TIME",
+            default="0",
+            help="time when statistics shall begin (default is from the beginning)",
+        )
+        subparser.add_argument(
+            "--to",
+            metavar="END_TIME",
+            default="now",
+            help="time when statistics shall end (default is now/latest)",
+        )
+        subparser.add_argument(
+            "--minimum-ratio",
+            metavar="MIN_RATIO",
+            default=0.05,
+            type=float,
+            help="time when statistics shall end (default is now/latest)",
+        )
+        subparser.add_argument(
+            "locations",
+            metavar="[[USER@]SERVER::]PATH",
+            nargs=1,
+            help="location of repository where to show statistics",
+        )
+        return subparser
+
+    def connect(self):
+        conn_value = super().connect()
+        if conn_value.is_connection_ok():
+            self.repo = repository.Repo(
+                self.connected_locations[0],
+                self.values,
+                must_be_writable=False,
+                must_exist=True,
+                can_be_sub_path=False,
+            )
+        return conn_value
+
+    def check(self):
+        # we try to identify as many potential errors as possible before we
+        # return, so we gather all potential issues and return only the final
+        # result
+        ret_code = super().check()
+
+        # we verify that source repository is correct
+        ret_code |= self.repo.check()
+
+        return ret_code
+
+    def setup(self):
+        # in setup we return as soon as we detect an issue to avoid changing
+        # too much
+        ret_code = super().setup()
+        if ret_code & Globals.RET_CODE_ERR:
+            return ret_code
+
+        ret_code = self.repo.setup()
+        if ret_code & Globals.RET_CODE_ERR:
+            return ret_code
+
+        self.from_time = self.repo.get_parsed_time(self.values["from"])
+        if self.from_time is None:
+            return Globals.RET_CODE_ERR
+
+        self.to_time = self.repo.get_parsed_time(self.values["to"])
+        if self.to_time is None:
+            return Globals.RET_CODE_ERR
+
+        return Globals.RET_CODE_OK
+
+    def run(self):
+        ret_code = super().run()
+        if ret_code & Globals.RET_CODE_ERR:
+            return ret_code
+
+        ret_code |= self.repo.get_statistics(
+            self.from_time, self.to_time, self.values["minimum_ratio"]
+        )
+
+        return ret_code
+
+
+def get_plugin_class():
+    return StatsAction
