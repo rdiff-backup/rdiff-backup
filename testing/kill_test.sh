2806153de474cd3d05bc4bf2e41dc5b834bab249
Eric L. <ewl+git@lavar.de>
2024-11-29 06:05:32 +0100

    Add generic testing script with explanations for future needs

--- testing/kill_test.sh
+++
@@ -1,6 +0,0 @@
-# script to test with specific verbosity and parameters
-BASEDIR="$(dirname $0)"
-TESTNAME="$(basename $0 .sh)"
-export RDIFF_BACKUP_VERBOSITY=${1}
-shift
-python "${BASEDIR}/${TESTNAME}.py" "${@}"
