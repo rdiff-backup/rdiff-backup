0aec9fc7200454a1319c9bd47702c6896e909a79
Eric L. <ericzolf@users.noreply.github.com>
2025-11-24 07:49:41 +0100

    Improve create_venv tool (#1086)
    * make it possible to choose base Python to use
    * fix confusion between dnf and yum

--- tools/create_venv.sh
+++ tools/create_venv.sh
@@ -2,12 +2,13 @@
 # create a Python virtualenv to test rdiff-backup
 # Set the variable BINDEP to a profile to have also binary packages installed
 # Possible profiles are: usage, devel
-# e.g. BINDEP=devel ./create_venv.sh my/venv/dir
+# e.g. BINDEP=devel ./create_venv.sh my/venv/dir python3.12
 
 VENV=${1:-dist/rdb}
+PYTHON=${2:-python3}
 mkdir -p $(dirname ${VENV})
-python -m venv ${VENV}
-python -m venv --upgrade --upgrade-deps ${VENV}
+${PYTHON} -m venv ${VENV}
+${PYTHON} -m venv --upgrade --upgrade-deps ${VENV}
 source ${VENV}/bin/activate
 if [[ -n "${BINDEP}" ]]
 then  # install binary dependencies e.g. to compile rdiff-backup
@@ -16,10 +17,10 @@ then  # install binary dependencies e.g. to compile rdiff-backup
 	if [[ -n "${packages}" ]]
 	then  # there are binary packages to install
 		echo "NOTE:    sudo password and confirmation might be necessary"
-		if [[ -n "$(which yum)" ]]
+		if [[ -n "$(which dnf)" ]]
 		then  # Fedora, RHEL, CentOS, etc
 			sudo dnf install ${packages}
-		elif [[ -n "$(which dnf)" ]]
+		elif [[ -n "$(which yum)" ]]
 		then  # Fedora, RHEL, CentOS (old)
 			sudo yum install ${packages}
 		elif [[ -n "$(which apt)" ]]
